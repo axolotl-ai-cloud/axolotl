99be0ad0e12d72460de4b50cd0661895fb18a55d
Wing Lian <wing.lian@gmail.com>
2023-04-18 02:48:43 -0400

    add support for alpaca reflect training

--- src/axolotl/prompt_tokenizers.py
+++ src/axolotl/prompt_tokenizers.py
@@ -107,6 +107,67 @@ class GPTeacherPromptTokenizingStrategy(InstructionPromptTokenizingStrategy):
         )
 
 
+class ReflectionPromptTokenizingStrategy(PromptTokenizingStrategy):
+    def parse_instruction_fields(self, prompt) -> (str, str, str, str, str):
+        raise NotImplementedError
+
+    def tokenize_prompt(self, prompt):
+        instruction, input, output, reflection, corrected = self.parse_instruction_fields(prompt)
+        full_prompt = self._build_full_prompt(instruction, input, output, reflection, corrected)
+        tokenized_full_prompt = self._tokenize(full_prompt)
+        if not self.train_on_inputs:
+            user_prompt = self.prompter.build_prompt(
+                instruction,
+                input,
+            )
+            tokenized_user_prompt = self._tokenize(user_prompt, add_eos_token=False)
+            user_prompt_len = len(tokenized_user_prompt["input_ids"])
+            # TODO this could be sped up using numpy array slicing
+            tokenized_full_prompt["labels"] = [
+                -100
+            ] * user_prompt_len + tokenized_full_prompt["labels"][user_prompt_len:]
+
+        return tokenized_full_prompt
+
+    def _build_full_prompt(self, instruction, input, output, reflection, corrected):
+        return self.prompter.build_prompt(
+            instruction,
+            input,
+            output,
+            reflection,
+            corrected,
+        )
+
+    def _tokenize(self, prompt, add_eos_token=True):
+        result = self.tokenizer(
+            prompt,
+            truncation=True,
+            max_length=self.sequence_len,
+            padding=False,
+            return_tensors=None,
+        )
+        if (
+            result["input_ids"][-1] != self.tokenizer.eos_token_id
+            and len(result["input_ids"]) < self.sequence_len
+            and add_eos_token
+        ):
+            result["input_ids"].append(self.tokenizer.eos_token_id)
+            result["attention_mask"].append(1)
+
+        result["labels"] = result["input_ids"].copy()
+        return result
+
+
+class AlpacaReflectionPTStrategy(ReflectionPromptTokenizingStrategy):
+    def parse_instruction_fields(self, prompt) -> (str, str, str, str, str):
+        return (
+            prompt["instruction"],
+            prompt["input"] if "input" in prompt else "",
+            prompt["output"],
+            prompt["reflection"],
+            prompt["corrected"],
+        )
+
 class ShareGPTPromptTokenizingStrategy(PromptTokenizingStrategy):
     def tokenize_prompt(self, prompt):
         try:
