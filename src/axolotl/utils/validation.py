a5bf83868512eee7bed8f4208be22ec4b858af87
Wing Lian <wing.lian@gmail.com>
2023-05-26 00:09:55 -0400

    add logging and make sure model unloads to float16

--- src/axolotl/utils/validation.py
+++ src/axolotl/utils/validation.py
@@ -1,3 +1,6 @@
+import logging
+
+
 def validate_config(cfg):
     if cfg.adapter == "qlora":
         if cfg.merge_lora:
@@ -9,6 +12,9 @@ def validate_config(cfg):
             assert cfg.load_in_8bit is False
             assert cfg.load_4bit is False
             assert cfg.load_in_4bit is True
+    if cfg.load_in_8bit and cfg.adapter == "lora":
+        logging.warning("we recommend setting `load_in_8bit: true`")
+
     # TODO
     # MPT 7b
     # https://github.com/facebookresearch/bitsandbytes/issues/25
