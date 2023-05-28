7bf2069afd349e1e049b2086c5cbc02b5ab0430e
NanoCode012 <kevinvong@rocketmail.com>
2023-05-28 23:14:04 +0900

    Apply black formatter

--- src/axolotl/utils/dict.py
+++ src/axolotl/utils/dict.py
@@ -2,8 +2,9 @@ from addict import Dict
 
 
 class DictDefault(Dict):
-    '''
+    """
     A Dict that returns None instead of returning empty Dict for missing keys.
-    '''
+    """
+
     def __missing__(self, key):
         return None
