05ab9092e304f234801d6496cecb60d49d86c0a4
Wing Lian <wing.lian@gmail.com>
2023-06-25 22:40:50 -0400

    skip the system prompt

--- src/axolotl/prompt_strategies/alpaca_instruct.py
+++ src/axolotl/prompt_strategies/alpaca_instruct.py
@@ -1,12 +1,21 @@
 """Module loading the AlpacaInstructPromptTokenizingStrategy class"""
 
 from axolotl.prompt_tokenizers import AlpacaPromptTokenizingStrategy
-from axolotl.prompters import AlpacaPrompter, PromptStyle
+from axolotl.prompters import AlpacaPrompter, PromptStyle, UnpromptedPrompter
 
 
 def load(tokenizer, cfg):
     return AlpacaPromptTokenizingStrategy(
         AlpacaPrompter(PromptStyle.INSTRUCT.value),
+        tokenizer,
+        cfg.train_on_inputs,
+        cfg.sequence_len,
+    )
+
+
+def load_no_prompt(tokenizer, cfg):
+    return AlpacaPromptTokenizingStrategy(
+        UnpromptedPrompter(PromptStyle.INSTRUCT.value),
         tokenizer,
         cfg.train_on_inputs,
         cfg.sequence_len,
