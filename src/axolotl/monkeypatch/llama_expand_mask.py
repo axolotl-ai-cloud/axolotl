29c236cd847c7dab54417be6344775e681525a45
Wing Lian <wing.lian@gmail.com>
2023-07-17 06:17:28 -0400

    fix expand mask for multiple batch items, make sure we pad position_ids

--- src/axolotl/monkeypatch/llama_expand_mask.py
+++ src/axolotl/monkeypatch/llama_expand_mask.py
@@ -10,21 +10,41 @@ def _expand_mask(mask: torch.Tensor, dtype: torch.dtype, tgt_len: Optional[int] 
     """
     Expands attention_mask from `[bsz, seq_len]` to `[bsz, 1, tgt_seq_len, src_seq_len]`.
     """
+    # Move the mask to the CPU
+    mask = mask.cpu()
+
     bsz, src_len = mask.size()
     tgt_len = tgt_len if tgt_len is not None else src_len
 
-    binary_mask = torch.where(
-        mask != 0, torch.tensor(1).to(torch.int16), torch.tensor(0).to(torch.int16)
-    )
+    # Initialize a tensor to hold the expanded masks
+    expanded_masks = torch.zeros(bsz, 1, tgt_len, src_len).to(dtype)
 
-    zero_one_mask = torch.eq(mask, mask.t()).int() * binary_mask
-    expanded_mask = zero_one_mask.unsqueeze(0).expand(bsz, 1, tgt_len, src_len)
+    # For each sequence in the batch
+    for i in range(bsz):
+        # Get the mask for this sequence
+        mask_i = mask[i].unsqueeze(0)
 
-    inverted_mask = 1.0 - expanded_mask
+        # Create a binary mask from the original mask where zeros remain zeros and all other values are set to one
+        binary_mask_i = torch.where(
+            mask_i != 0,
+            torch.tensor(1).to(dtype).cpu(),
+            torch.tensor(0).to(dtype).cpu(),
+        )
+
+        # Create a block-diagonal mask
+        zero_one_mask_i = torch.eq(mask_i, mask_i.t()).int() * binary_mask_i
+
+        # Expand the mask
+        expanded_mask_i = zero_one_mask_i.unsqueeze(0).expand(1, 1, tgt_len, src_len)
+
+        # Store the expanded mask
+        expanded_masks[i] = expanded_mask_i
+
+    inverted_mask = 1.0 - expanded_masks
 
     return inverted_mask.masked_fill(
         inverted_mask.to(torch.bool), torch.finfo(dtype).min
-    )
+    ).cpu()
 
 
 def hijack_expand_mask():
