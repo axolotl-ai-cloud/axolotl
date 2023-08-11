5ac7ef4b1c9726e6cb283cfb64758098da70ac55
Aman Karmani <aman@tmm1.net>
2023-08-11 17:12:13 +0000

    improve gpu logging to include reserved pytorch memory and other kernels and overhead

--- src/axolotl/utils/bench.py
+++ src/axolotl/utils/bench.py
@@ -5,12 +5,15 @@ import torch
 
 
 def gpu_memory_usage(device=0):
+    return torch.cuda.memory_allocated(device) / 1024.0**3
+
+
+def gpu_memory_usage_smi(device=0):
     if isinstance(device, torch.device):
         device = device.index
     if isinstance(device, str) and device.startswith("cuda:"):
         device = int(device[5:])
 
-    # NB torch.cuda.memory_usage returns zero so we use lower level api
     pynvml.nvmlInit()
     handle = pynvml.nvmlDeviceGetHandleByIndex(device)
     info = pynvml.nvmlDeviceGetMemoryInfo(handle)
@@ -19,5 +22,14 @@ def gpu_memory_usage(device=0):
 
 def log_gpu_memory_usage(log, msg, device):
     usage = gpu_memory_usage(device)
-    log.info(f"GPU memory usage {msg}: {usage:.03f} GB", stacklevel=2)
+    extras = []
+    reserved = torch.cuda.memory_reserved(device) / 1024.0**3
+    if reserved > usage:
+        extras.append(f"+{reserved-usage:.03f}GB cache")
+    smi = gpu_memory_usage_smi(device)
+    if smi > reserved:
+        extras.append(f"+{smi-reserved:.03f}GB misc")
+    log.info(
+        f"GPU memory usage {msg}: {usage:.03f}GB ({', '.join(extras)})", stacklevel=2
+    )
     return usage
