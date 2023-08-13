15f6e57eaa6f003d12a8f403d2c6fe23610e76a3
Charles Goddard <chargoddard@gmail.com>
2023-08-13 13:19:48 -0700

    Fix crash when running without CUDA

--- src/axolotl/utils/bench.py
+++ src/axolotl/utils/bench.py
@@ -28,6 +28,9 @@ def gpu_memory_usage_smi(device=0):
 
 
 def log_gpu_memory_usage(log, msg, device):
+    if not torch.cuda.is_available():
+        return (0, 0, 0)
+
     usage, cache, misc = gpu_memory_usage_all(device)
     extras = []
     if cache > 0:
