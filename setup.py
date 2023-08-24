ac37753aa27e76c0c30de402d81e78708b36dceb
Wing Lian <wing.lian@gmail.com>
2023-08-23 21:38:45 -0400

    remove old gptq docker

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_requires = []
 with open("./requirements.txt", encoding="utf-8") as requirements_file:
     # don't include peft yet until we check the int4
     # need to manually install peft for now...
-    reqs = [r.strip() for r in requirements_file.readlines() if "peft" not in r]
+    reqs = [r.strip() for r in requirements_file.readlines()]
     reqs = [r for r in reqs if "flash-attn" not in r]
     reqs = [r for r in reqs if r and r[0] != "#"]
     for r in reqs:
@@ -20,20 +20,11 @@ setup(
     packages=find_packages(),
     install_requires=install_requires,
     extras_require={
-        "gptq": [
-            "alpaca_lora_4bit @ git+https://github.com/winglian/alpaca_lora_4bit.git@setup_pip",
-        ],
-        "gptq_triton": [
-            "alpaca_lora_4bit[triton] @ git+https://github.com/winglian/alpaca_lora_4bit.git@setup_pip",
-        ],
         "flash-attn": [
             "flash-attn==2.0.8",
         ],
         "extras": [
             "deepspeed",
-        ],
-        "peft": [
-            "peft @ git+https://github.com/huggingface/peft.git",
         ],
     },
 )
