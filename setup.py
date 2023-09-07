282e4dbcbea9d867e2fb8cc6f693438e2a000375
Wing Lian <wing.lian@gmail.com>
2023-09-07 08:46:07 -0400

    default install flash-attn and auto-gptq now too

--- setup.py
+++ setup.py
@@ -7,15 +7,13 @@ def parse_requirements():
     _install_requires = []
     _dependency_links = []
     with open("./requirements.txt", encoding="utf-8") as requirements_file:
-        lines = [
-            r.strip() for r in requirements_file.readlines() if "auto-gptq" not in r
-        ]
+        lines = [r.strip() for r in requirements_file.readlines()]
         for line in lines:
             if line.startswith("--extra-index-url"):
                 # Handle custom index URLs
                 _, url = line.split()
                 _dependency_links.append(url)
-            elif "flash-attn" not in line and line and line[0] != "#":
+            elif line and line[0] != "#":
                 # Handle standard packages
                 _install_requires.append(line)
     return _install_requires, _dependency_links
@@ -33,12 +31,6 @@ setup(
     install_requires=install_requires,
     dependency_links=dependency_links,
     extras_require={
-        "gptq": [
-            "auto-gptq",
-        ],
-        "flash-attn": [
-            "flash-attn==2.0.8",
-        ],
         "extras": [
             "deepspeed",
         ],
