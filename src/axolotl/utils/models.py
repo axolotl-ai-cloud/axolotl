79b3fe56336b446c3be3aded9cf07eef0d3a242e
Wing Lian <wing.lian@gmail.com>
2023-09-13 12:47:40 -0400

    fix var

--- src/axolotl/utils/models.py
+++ src/axolotl/utils/models.py
@@ -110,11 +110,11 @@ def load_model(
     # TODO refactor as a kwarg
     load_in_8bit = cfg.load_in_8bit
 
-    if hasattr(model_config, "model_type") and model_type.model_type == "btlm":
+    if hasattr(model_config, "model_type") and model_config.model_type == "btlm":
         if cfg.flash_attention:
             replace_btlm_attn_with_flash_attn()
 
-    if hasattr(model_config, "model_type") and model_type.model_type == "falcon":
+    if hasattr(model_config, "model_type") and model_config.model_type == "falcon":
         if cfg.flash_attention:
             replace_falcon_attn_with_flash_attn()
 
