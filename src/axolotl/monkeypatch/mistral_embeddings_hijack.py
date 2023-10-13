08147f786b4da8081e5474b305eb155964c9c855
Maxime <maxime@nope.no>
2023-10-13 11:48:56 +0000

    fix format

--- src/axolotl/monkeypatch/mistral_embeddings_hijack.py
+++ src/axolotl/monkeypatch/mistral_embeddings_hijack.py
@@ -1,8 +1,9 @@
 import torch
-from transformers.utils import logging
 import transformers.models.mistral.modeling_mistral
+from transformers.utils import logging
 
 logger = logging.get_logger(__name__)
+
 
 def replace_mistral_embeddings_with_uniform_distribution(noise_alpha=5):
     def noised_embed(orig_embed, noise_alpha, model):
@@ -12,16 +13,24 @@ def replace_mistral_embeddings_with_uniform_distribution(noise_alpha=5):
             if model.training:
                 embed_init = orig_embed(x)
                 dims = torch.tensor(embed_init.size(1) * embed_init.size(2))
-                mag_norm = noise_alpha/torch.sqrt(dims)
-                return embed_init + torch.zeros_like(embed_init).uniform_(-mag_norm, mag_norm)
+                mag_norm = noise_alpha / torch.sqrt(dims)
+                return embed_init + torch.zeros_like(embed_init).uniform_(
+                    -mag_norm, mag_norm
+                )
             else:
                 return orig_embed(x)
+
         return new_func
-    
+
     def post_init(orig_post_init):
         def new_func(self):
             orig_post_init(self)
-            self.embed_tokens.forward = noised_embed(self.embed_tokens.forward, noise_alpha, self)
+            self.embed_tokens.forward = noised_embed(
+                self.embed_tokens.forward, noise_alpha, self
+            )
+
         return new_func
 
-    transformers.models.mistral.modeling_mistral.MistralModel.post_init = post_init(transformers.models.mistral.modeling_mistral.MistralModel.post_init)
+    transformers.models.mistral.modeling_mistral.MistralModel.post_init = post_init(
+        transformers.models.mistral.modeling_mistral.MistralModel.post_init
+    )
