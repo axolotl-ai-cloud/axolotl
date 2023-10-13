779466d3ffec12945d036900e04108e1c4767fc5
Maxime <maxime@nope.no>
2023-10-13 12:06:34 +0000

    linter issues

--- src/axolotl/monkeypatch/llama_embeddings_hijack.py
+++ src/axolotl/monkeypatch/llama_embeddings_hijack.py
@@ -7,18 +7,17 @@ logger = logging.get_logger(__name__)
 
 def replace_llama_embeddings_with_uniform_distribution(noise_alpha=5):
     def noised_embed(orig_embed, noise_alpha, model):
-        def new_func(x):
+        def new_func(input_ids):
             # during training, we add noise to the embedding
             # during generation, we don't add noise to the embedding
             if model.training:
-                embed_init = orig_embed(x)
+                embed_init = orig_embed(input_ids)
                 dims = torch.tensor(embed_init.size(1) * embed_init.size(2))
                 mag_norm = noise_alpha / torch.sqrt(dims)
                 return embed_init + torch.zeros_like(embed_init).uniform_(
                     -mag_norm, mag_norm
                 )
-            else:
-                return orig_embed(x)
+            return orig_embed(input_ids)
 
         return new_func
 
