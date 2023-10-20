3e34c4c024d16b9ab01cb37450e83430284e4da4
Wing Lian <wing.lian@gmail.com>
2023-10-20 18:03:31 -0400

    no lora for tests

--- tests/e2e/test_fused_llama.py
+++ tests/e2e/test_fused_llama.py
@@ -38,11 +38,6 @@ class TestFusedLlama(unittest.TestCase):
                 "sample_packing": True,
                 "sequence_len": 1024,
                 "load_in_8bit": True,
-                "adapter": "lora",
-                "lora_r": 32,
-                "lora_alpha": 64,
-                "lora_dropout": 0.05,
-                "lora_target_linear": True,
                 "val_set_size": 0.1,
                 "special_tokens": {
                     "unk_token": "<unk>",
@@ -72,7 +67,7 @@ class TestFusedLlama(unittest.TestCase):
         dataset_meta = load_datasets(cfg=cfg, cli_args=cli_args)
 
         train(cfg=cfg, cli_args=cli_args, dataset_meta=dataset_meta)
-        assert (Path(output_dir) / "adapter_model.bin").exists()
+        assert (Path(output_dir) / "pytorch_model.bin").exists()
 
     def test_fft_packing(self):
         # pylint: disable=duplicate-code
