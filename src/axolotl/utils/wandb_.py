bcd588220d994a1eb23b0883e577386a6667813a
NanoCode012 <kevinvong@rocketmail.com>
2023-10-22 23:35:07 +0900

    fix: indent and pop disabled env if project passed

--- src/axolotl/utils/wandb_.py
+++ src/axolotl/utils/wandb_.py
@@ -10,11 +10,12 @@ def setup_wandb_env_vars(cfg: DictDefault):
         if key.startswith("wandb_"):
             value = cfg.get(key, "")
 
-        if value and len(value) > 0:
-            os.environ[key.upper()] = value
+            if value and isinstance(value, str) and len(value) > 0:
+                os.environ[key.upper()] = value
 
     # Enable wandb if project name is present
     if cfg.wandb_project and len(cfg.wandb_project) > 0:
         cfg.use_wandb = True
+        os.environ.pop("WANDB_DISABLED", None)  # Remove if present
     else:
         os.environ["WANDB_DISABLED"] = "true"
