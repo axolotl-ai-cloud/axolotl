4c1a16f73cfbd2836187193d0cfe76f12199502c
Casper <casperbh.96@gmail.com>
2023-12-06 22:33:41 +0100

    Pin flash-attn to 2.3.3

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     dependency_links=dependency_links,
     extras_require={
         "flash-attn": [
-            "flash-attn>=2.3.0",
+            "flash-attn==2.3.3",
         ],
         "deepspeed": [
             "deepspeed",
