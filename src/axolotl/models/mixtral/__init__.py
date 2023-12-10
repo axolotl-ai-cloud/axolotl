279a1401b5237ef3ff01fb4ff89b34fb02197372
Casper <casperbh.96@gmail.com>
2023-12-10 17:23:06 +0100

    Formatting again

--- src/axolotl/models/mixtral/__init__.py
+++ src/axolotl/models/mixtral/__init__.py
@@ -3,7 +3,7 @@ Custom modeling code for mixtral
 """
 
 from .configuration_moe_mistral import MixtralConfig  # noqa
-from .modeling_moe_mistral import ( # noqa
+from .modeling_moe_mistral import (  # noqa
     MixtralForCausalLM,
     replace_mixtral_mlp_with_swiglu,
 )
