e44a85219d4f492c8867ccfa0aff5ac20c627bdf
hamelsmu <hamel.husain@gmail.com>
2023-12-14 09:25:33 +0000

    apply black

--- src/axolotl/monkeypatch/fastchat_conversation_turns.py
+++ src/axolotl/monkeypatch/fastchat_conversation_turns.py
@@ -83,19 +83,19 @@ def get_turns(  # pylint: disable=too-many-return-statements
                 yield role + ":", ""
         return
     if self.sep_style == SeparatorStyle.LLAMA2:
-        seps = [self.sep, self.sep2]
         if self.system_message:
-            if (
-                self.messages
-            ):  # For llama, the system message is incorporated into the first human instruction
+            if self.messages:
+                # For llama, the system message is incorporated into the first human instruction
                 system_prompt += self.messages[0][1]
                 self.messages.pop(0)
             yield "", system_prompt
-        else:
-            yield "", "[INST] "
         for i, (role, message) in enumerate(self.messages):
             if message:
-                yield role + " ", message + seps[i % 2]
+                if (i % 2 == 0 and not self.system_message) or (
+                    i % 2 != 0 and self.system_message
+                ):
+                    role = "<s> " + role
+                yield role + " ", message
             else:
                 yield role, ""
         return
