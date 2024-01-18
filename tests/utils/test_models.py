e6e67dd174efa50c4493e1f4b6cc1dcbdab2e2c3
Wing Lian <wing.lian@gmail.com>
2024-01-18 04:41:32 -0500

    reduce batch size for larger context len and udpate test to check message

--- tests/utils/test_models.py
+++ tests/utils/test_models.py
@@ -28,6 +28,10 @@ class ModelsUtilsTest(unittest.TestCase):
             "axolotl.utils.models.load_model_config"
         ) as mocked_load_model_config:
             mocked_load_model_config.return_value = {}
-            with pytest.raises(ValueError):
+            with pytest.raises(ValueError) as exc:
                 # Should error before hitting tokenizer, so we pass in an empty str
                 load_model(cfg, tokenizer="")
+            assert (
+                "shifted-sparse attention does not currently support sample packing"
+                in exc.value.message
+            )
