929933fa239a2efd8223ca9fe730727feb6c2c92
Wing Lian <wing.lian@gmail.com>
2024-01-23 09:39:32 -0500

    test qlora too

--- tests/e2e/patched/test_phi_multipack.py
+++ tests/e2e/patched/test_phi_multipack.py
@@ -33,8 +33,9 @@ class TestPhiMultipack(unittest.TestCase):
                 "trust_remote_code": True,
                 "model_type": "PhiForCausalLM",
                 "tokenizer_type": "AutoTokenizer",
-                "sequence_len": 1024,
+                "sequence_len": 2048,
                 "sample_packing": True,
+                "pad_to_sequence_len": True,
                 "load_in_8bit": False,
                 "adapter": None,
                 "val_set_size": 0.1,
@@ -69,3 +70,54 @@ class TestPhiMultipack(unittest.TestCase):
 
         train(cfg=cfg, cli_args=cli_args, dataset_meta=dataset_meta)
         assert (Path(temp_dir) / "pytorch_model.bin").exists()
+
+    @with_temp_dir
+    def test_qlora_packed(self, temp_dir):
+        # pylint: disable=duplicate-code
+        cfg = DictDefault(
+            {
+                "base_model": "microsoft/phi-1_5",
+                "trust_remote_code": True,
+                "model_type": "PhiForCausalLM",
+                "tokenizer_type": "AutoTokenizer",
+                "sequence_len": 2048,
+                "sample_packing": True,
+                "pad_to_sequence_len": True,
+                "load_in_8bit": False,
+                "adapter": "qlora",
+                "lora_r": 64,
+                "lora_alpha": 32,
+                "lora_dropout": 0.05,
+                "lora_target_linear": True,
+                "val_set_size": 0.1,
+                "special_tokens": {
+                    "pad_token": "<|endoftext|>",
+                },
+                "datasets": [
+                    {
+                        "path": "mhenrichsen/alpaca_2k_test",
+                        "type": "alpaca",
+                    },
+                ],
+                "dataset_shard_num": 10,
+                "dataset_shard_idx": 0,
+                "num_epochs": 1,
+                "micro_batch_size": 1,
+                "gradient_accumulation_steps": 1,
+                "output_dir": temp_dir,
+                "learning_rate": 0.00001,
+                "optimizer": "adamw_bnb_8bit",
+                "lr_scheduler": "cosine",
+                "max_steps": 20,
+                "eval_steps": 10,
+                "save_steps": 10,
+                "bf16": "auto",
+            }
+        )
+
+        normalize_config(cfg)
+        cli_args = TrainerCliArgs()
+        dataset_meta = load_datasets(cfg=cfg, cli_args=cli_args)
+
+        train(cfg=cfg, cli_args=cli_args, dataset_meta=dataset_meta)
+        assert (Path(temp_dir) / "adapter_model.bin").exists()
--- tests/e2e/test_phi.py
+++ tests/e2e/test_phi.py
@@ -25,7 +25,7 @@ class TestPhi(unittest.TestCase):
     """
 
     @with_temp_dir
-    def test_phi2_ft(self, temp_dir):
+    def test_phi_ft(self, temp_dir):
         # pylint: disable=duplicate-code
         cfg = DictDefault(
             {
@@ -33,7 +33,7 @@ class TestPhi(unittest.TestCase):
                 "trust_remote_code": True,
                 "model_type": "AutoModelForCausalLM",
                 "tokenizer_type": "AutoTokenizer",
-                "sequence_len": 512,
+                "sequence_len": 2048,
                 "sample_packing": False,
                 "load_in_8bit": False,
                 "adapter": None,
@@ -60,7 +60,6 @@ class TestPhi(unittest.TestCase):
                 "max_steps": 10,
                 "save_steps": 10,
                 "eval_steps": 10,
-                "save_safetensors": True,
                 "bf16": "auto",
             }
         )
@@ -70,3 +69,53 @@ class TestPhi(unittest.TestCase):
 
         train(cfg=cfg, cli_args=cli_args, dataset_meta=dataset_meta)
         assert (Path(temp_dir) / "pytorch_model.bin").exists()
+
+    @with_temp_dir
+    def test_phi_qlora(self, temp_dir):
+        # pylint: disable=duplicate-code
+        cfg = DictDefault(
+            {
+                "base_model": "microsoft/phi-1_5",
+                "trust_remote_code": True,
+                "model_type": "AutoModelForCausalLM",
+                "tokenizer_type": "AutoTokenizer",
+                "sequence_len": 2048,
+                "sample_packing": False,
+                "load_in_8bit": False,
+                "adapter": "qlora",
+                "lora_r": 64,
+                "lora_alpha": 32,
+                "lora_dropout": 0.05,
+                "lora_target_linear": True,
+                "val_set_size": 0.1,
+                "special_tokens": {
+                    "pad_token": "<|endoftext|>",
+                },
+                "datasets": [
+                    {
+                        "path": "mhenrichsen/alpaca_2k_test",
+                        "type": "alpaca",
+                    },
+                ],
+                "dataset_shard_num": 10,
+                "dataset_shard_idx": 0,
+                "num_epochs": 1,
+                "micro_batch_size": 1,
+                "gradient_accumulation_steps": 1,
+                "output_dir": temp_dir,
+                "learning_rate": 0.00001,
+                "optimizer": "paged_adamw_8bit",
+                "lr_scheduler": "cosine",
+                "flash_attention": True,
+                "max_steps": 10,
+                "save_steps": 10,
+                "eval_steps": 10,
+                "bf16": "auto",
+            }
+        )
+        normalize_config(cfg)
+        cli_args = TrainerCliArgs()
+        dataset_meta = load_datasets(cfg=cfg, cli_args=cli_args)
+
+        train(cfg=cfg, cli_args=cli_args, dataset_meta=dataset_meta)
+        assert (Path(temp_dir) / "adapter_model.bin").exists()
