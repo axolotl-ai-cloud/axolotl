1427d5b502e98ec5037617d4d2f902c9a88643c2
Wing Lian <wing.lian@gmail.com>
2024-01-24 15:00:28 -0500

    prepare for release 0.4.0 (#1175)

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ install_requires, dependency_links = parse_requirements()
 
 setup(
     name="axolotl",
-    version="0.3.0",
+    version="0.4.0",
     description="LLM Trainer",
     long_description="Axolotl is a tool designed to streamline the fine-tuning of various AI models, offering support for multiple configurations and architectures.",
     package_dir={"": "src"},
