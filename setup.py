bf0f14d12eaec472f945543d3e67e09f0d1dac99
Damien Tanner <damien@tanner.me>
2024-02-01 16:23:25 +0000

    fix xformers version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def parse_requirements():
         torch_version = version("torch")
         if torch_version.startswith("2.1.1"):
             _install_requires.pop(_install_requires.index("xformers==0.0.22"))
-            _install_requires.append("xformers==0.0.23")
+            _install_requires.append("xformers>=0.0.23")
     except PackageNotFoundError:
         pass
 
