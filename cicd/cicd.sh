d62e189222f0b52664bb0693b486764543ce7b2a
Wing Lian <wing.lian@gmail.com>
2024-02-13 14:32:28 -0800

    reorder tests to run on modal

--- cicd/cicd.sh
+++ cicd/cicd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pytest --ignore=tests/e2e/ /workspace/axolotl/tests/
-pytest --ignore=tests/e2e/patched/ /workspace/axolotl/tests/e2e/
 pytest /workspace/axolotl/tests/e2e/patched/
+pytest --ignore=tests/e2e/patched/ /workspace/axolotl/tests/e2e/
