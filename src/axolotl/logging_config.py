59dbfa9d560ab7927a453237779777e2f2762b3b
Chirag Jain <jain.chirag925@gmail.com>
2024-03-12 00:54:06 +0530

    Don't disable existing loggers when configuring axolotl logging

--- src/axolotl/logging_config.py
+++ src/axolotl/logging_config.py
@@ -30,6 +30,7 @@ class ColorfulFormatter(Formatter):
 
 DEFAULT_LOGGING_CONFIG: Dict[str, Any] = {
     "version": 1,
+    "disable_existing_loggers": False,
     "formatters": {
         "simple": {
             "format": "[%(asctime)s] [%(levelname)s] [%(name)s.%(funcName)s:%(lineno)d] [PID:%(process)d] %(message)s",
