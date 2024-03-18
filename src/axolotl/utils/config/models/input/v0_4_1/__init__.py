2ea70ebbd8f1d8d46e692afd05773dcf06626601
Wing Lian <wing.lian@gmail.com>
2024-03-18 13:10:00 -0400

    ORPO (#1419)
    * orpo trainer
    
    * rl handling for orpo
    
    * support for remove_unused_columns
    
    * orpo fixes
    
    * fix loader for orpo
    
    * chore: lint
    
    * fix default for remove_unused_columns
    
    * roll ORPO into the main AxolotlTrainer so it can be compatible with some of the other techniques like relora
    
    * better handling of system message for orpo
    
    * revert system prompt changes for chat templtes
    
    * no need for else condition
    
    * split dataset parsing into it's own component

--- src/axolotl/utils/config/models/input/v0_4_1/__init__.py
+++ src/axolotl/utils/config/models/input/v0_4_1/__init__.py
@@ -124,6 +124,7 @@ class RLType(str, Enum):
     dpo = "dpo"  # pylint: disable=invalid-name
     ipo = "ipo"  # pylint: disable=invalid-name
     kto_pair = "kto_pair"  # pylint: disable=invalid-name
+    orpo = "orpo"  # pylint: disable=invalid-name
 
 
 class ChatTemplate(str, Enum):
@@ -431,6 +432,8 @@ class AxolotlInputConfig(
     dataloader_prefetch_factor: Optional[int] = None
     dataloader_drop_last: Optional[bool] = None
 
+    remove_unused_columns: Optional[bool] = None
+
     push_dataset_to_hub: Optional[str] = None
     hf_use_auth_token: Optional[bool] = None
 
@@ -514,6 +517,8 @@ class AxolotlInputConfig(
     load_best_model_at_end: Optional[bool] = False
 
     neftune_noise_alpha: Optional[float] = None
+
+    orpo_alpha: Optional[float] = None
 
     max_memory: Optional[
         Dict[Union[int, Literal["cpu", "disk"]], Union[int, str]]
