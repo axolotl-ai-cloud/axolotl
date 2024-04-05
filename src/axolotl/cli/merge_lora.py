87ca3f98c62052490db63b4650301e47a2fdc8fa
Wing Lian <wing.lian@gmail.com>
2024-04-04 18:20:32 -0700

    don't use deepspeed or fsdp when merging loras (#1479)

--- src/axolotl/cli/merge_lora.py
+++ src/axolotl/cli/merge_lora.py
@@ -38,6 +38,8 @@ def do_cli(config: Path = Path("examples/"), **kwargs):
     parsed_cfg.load_in_4bit = False
     parsed_cfg.load_in_8bit = False
     parsed_cfg.flash_attention = False
+    parsed_cfg.deepspeed = None
+    parsed_cfg.fsdp = None
 
     do_merge_lora(cfg=parsed_cfg, cli_args=parsed_cli_args)
 
