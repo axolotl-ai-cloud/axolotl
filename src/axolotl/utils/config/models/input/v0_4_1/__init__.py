bf4cd672522729e5b0ad9a5523ad83a60de19243
NanoCode012 <kevinvong@rocketmail.com>
2024-04-05 12:47:32 +0900

    feat: validate sample packing requires flash_attention (#1465)
    * feat: validate sample packing requires flash_attention
    
    * fix: check for sdp_attn per suggestion
    
    * feat: add FA to tests

--- src/axolotl/utils/config/models/input/v0_4_1/__init__.py
+++ src/axolotl/utils/config/models/input/v0_4_1/__init__.py
@@ -1,6 +1,7 @@
 """
 Module for pydantic models for configuration
 """
+
 # pylint: disable=too-many-lines
 
 import logging
@@ -651,6 +652,20 @@ class AxolotlInputConfig(
         if data.get("sample_packing") and data.get("xformers_attention"):
             raise ValueError(
                 "sample_packing not compatible with xformers_attention. Use flash_attention"
+            )
+
+        return data
+
+    @model_validator(mode="before")
+    @classmethod
+    def check_sample_packing_wo_flash(cls, data):
+        if (
+            data.get("sample_packing")
+            and not data.get("flash_attention")
+            and not data.get("sdp_attention")
+        ):
+            raise ValueError(
+                "sample_packing requires flash_attention or sdp_attention to be set to true"
             )
 
         return data
