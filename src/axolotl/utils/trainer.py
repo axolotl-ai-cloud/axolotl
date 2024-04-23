aacdbc34daf367da0459fd64c42e47baf4edc4bd
Wing Lian <wing.lian@gmail.com>
2024-04-23 13:57:40 -0400

    fixes for pose splitting

--- src/axolotl/utils/trainer.py
+++ src/axolotl/utils/trainer.py
@@ -100,7 +100,10 @@ def add_position_ids(sample):
 
 
 def add_pose_position_ids(
-    sample, max_context_len=32768, split_on_token_ids: Optional[List[int]] = None
+    sample,
+    max_context_len=32768,
+    split_on_token_ids: Optional[List[int]] = None,
+    chunks: int = 2,
 ):
     """
     use the PoSE technique to extend the context length by randomly skipping
@@ -119,40 +122,42 @@ def add_pose_position_ids(
     if split_on_token_ids is None:
         split_on_token_ids = []
 
-    split_indices = [
-        i for i, token_id in enumerate(input_ids) if token_id in split_on_token_ids
-    ]
+    if split_on_token_ids:
+        split_indices = [
+            i for i, token_id in enumerate(input_ids) if token_id in split_on_token_ids
+        ]
+    else:
+        split_indices = [sample_len // chunks]
+    split_indices.append(len(input_ids))  # make sure we go to the end of the sample
     if split_indices[0] < 2:
         # drop the first split index if it's too close to the beginning
         split_indices = split_indices[1:]
 
-    if len(split_indices) == 0:
-        position_ids = torch.arange(sample_len)
-    else:
-        position_ids = []
-        prev_index = -1
-        total_skips = 0
+    position_ids = []
+    prev_index = 0
+    total_skips = 0
 
-        for split_index in split_indices:
-            num_skips = (
-                random.randint(0, max_skips)  # nosec B311
-                if prev_index != -1 and max_skips
-                else 0
-            )
-            max_skips -= num_skips
-            total_skips += num_skips
+    for split_index in split_indices:
+        num_skips = (
+            random.randint(0, max_skips)  # nosec B311
+            if prev_index != 0 and max_skips
+            else 0
+        )
+        max_skips -= num_skips
+        total_skips += num_skips
 
-            segment_position_ids = list(
-                range(prev_index + 1 + total_skips, split_index + total_skips)
-            )
+        segment_position_ids = list(
+            range(prev_index + total_skips, split_index + total_skips)
+        )
 
-            position_ids.extend(segment_position_ids)
-            prev_index = split_index
+        position_ids.extend(segment_position_ids)
+        prev_index = split_index
 
-        position_ids = torch.tensor(position_ids)
+    position_ids = torch.tensor(position_ids)
 
     sample["position_ids"] = position_ids
     sample["length"] = len(position_ids)
+    assert len(position_ids) == len(input_ids)
 
     return sample
 
@@ -162,8 +167,11 @@ def add_length(sample):
     return sample
 
 
-def drop_long_seq(sample, sequence_len=2048):
-    return len(sample["input_ids"]) <= sequence_len and len(sample["input_ids"]) > 0
+def drop_long_seq(sample, sequence_len=2048, min_sequence_len=2):
+    return (
+        len(sample["input_ids"]) <= sequence_len
+        and len(sample["input_ids"]) >= min_sequence_len
+    )
 
 
 def process_datasets_for_packing(cfg, train_dataset, eval_dataset):
