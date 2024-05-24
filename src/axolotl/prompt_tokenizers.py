4328429fc1237c681d3d37b60c15febdd7d83cfc
Artem Dinaburg <artem@trailofbits.com>
2024-05-24 17:08:30 -0400

    Fix tokenization for Qwen models
    Some models, like the Qwen series, return a token_type_ids along with
    input_ids and attention_mask from the tokenizer. In some cases, when
    finetuning in completion mode, Axolotl will add/strip EOS/BOS tokens.
    When this case was hit, token_type_ids would differ in length from
    input_ids, and the dataset could not be saved.
    
    This change makes Qwen models work by modifying token_type_ids along
    with the other values returned from the tokenizer.

--- src/axolotl/prompt_tokenizers.py
+++ src/axolotl/prompt_tokenizers.py
@@ -84,10 +84,14 @@ class PromptTokenizingStrategy(abc.ABC):
         ):
             result["input_ids"].append(self.tokenizer.eos_token_id)
             result["attention_mask"].append(1)
+            if "token_type_ids" in result:
+                result["token_type_ids"].append(result["token_type_ids"][-1])
 
         if result["input_ids"][0] == self.tokenizer.bos_token_id and strip_bos_token:
             result["input_ids"] = result["input_ids"][1:]
             result["attention_mask"] = result["attention_mask"][1:]
+            if "token_type_ids" in result:
+                result["token_type_ids"] = result["token_type_ids"][1:]
 
         result["labels"] = result["input_ids"].copy()
         return result
