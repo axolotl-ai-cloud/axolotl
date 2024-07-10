8fbc5826714e3c5ccf86d23cea5ff85df8c8b8f1
Wing Lian <wing.lian@gmail.com>
2024-07-10 10:24:22 -0400

    remove the bos token from dpo outputs

--- src/axolotl/core/trainer_builder.py
+++ src/axolotl/core/trainer_builder.py
@@ -1053,9 +1053,22 @@ class AxolotlDPOTrainer(SchedulerMixin, DPOTrainer):
             max_completion_length,
             add_special_tokens,
         )
+# fix when the tokenizer doesn't have a bos_token_id, e.g. Qwen
         if processing_class.bos_token_id is None and res["prompt_input_ids"][0] is None:
             for key in res.keys():
                 res[key] = res[key][1:]
+
+        if self.tokenizer.bos_token_id is not None:
+            # dpo trainer is incorrectly prepending the bos_token_id to the dpo outputs
+            if res["chosen_input_ids"][0] == self.tokenizer.bos_token_id:
+                res["chosen_input_ids"] = res["chosen_input_ids"][1:]
+                res["chosen_labels"] = res["chosen_labels"][1:]
+                res["chosen_attention_mask"] = res["chosen_attention_mask"][1:]
+            if res["rejected_input_ids"][0] == self.tokenizer.bos_token_id:
+                res["rejected_input_ids"] = res["rejected_input_ids"][1:]
+                res["rejected_labels"] = res["rejected_labels"][1:]
+                res["rejected_attention_mask"] = res["rejected_attention_mask"][1:]
+
         return res
 
     def training_step(
