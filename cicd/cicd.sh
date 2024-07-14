6a449cc63c033a43c9cf80ca1b2ac0f562bbd243
Wing Lian <wing.lian@gmail.com>
2024-07-14 13:44:22 -0400

    mark pytest as forked for patches

--- cicd/cicd.sh
+++ cicd/cicd.sh
@@ -2,5 +2,5 @@
 set -e
 
 pytest --ignore=tests/e2e/ /workspace/axolotl/tests/
-pytest -n auto /workspace/axolotl/tests/e2e/patched/
+pytest /workspace/axolotl/tests/e2e/patched/
 pytest --ignore=tests/e2e/patched/ /workspace/axolotl/tests/e2e/
