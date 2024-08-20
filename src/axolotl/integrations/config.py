f3334c7b8f3eb34a78e8994b5be213aad733f894
Wing Lian <wing.lian@gmail.com>
2024-08-20 19:54:26 -0400

    fix licensing so liger integration is Apache 2.0

--- src/axolotl/integrations/config.py
+++ src/axolotl/integrations/config.py
@@ -1,3 +1,15 @@
+# Copyright 2024 Axolotl AI. All rights reserved.
+#
+# This software may be used and distributed according to
+# the terms of the Axolotl Community License Agreement (the "License");
+# you may not use this file except in compliance with the License.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
 """
 module to handle merging the plugins' input arguments with the base configurations.
 
