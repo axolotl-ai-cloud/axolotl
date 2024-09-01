52cdd8997371fe7c750cfb9f9b020dc217006d81
Wing Lian <wing.lian@gmail.com>
2024-08-31 22:19:36 -0400

    add e2e smoke tests for llama liger integration

--- cicd/cicd.sh
+++ cicd/cicd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
-pytest --ignore=tests/e2e/ /workspace/axolotl/tests/
+pytest --ignore=tests/e2e/ --ignore=tests/integrations/e2e/ /workspace/axolotl/tests/
 pytest -n1 --dist loadfile -v /workspace/axolotl/tests/e2e/patched/
+pytest -n1 --dist loadfile -v /workspace/axolotl/tests/integrations/e2e/
 pytest --ignore=tests/e2e/patched/ --ignore=tests/e2e/multigpu/ /workspace/axolotl/tests/e2e/
