c1286c36f0dc7fd2890f09f3b14e1729c4cd342d
Wing Lian <wing.lian@gmail.com>
2024-10-02 09:34:15 -0400

    more places when skipping preprocess

--- src/axolotl/utils/trainer.py
+++ src/axolotl/utils/trainer.py
@@ -306,7 +306,7 @@ def process_pretraining_datasets_for_packing(
 
 
 def calculate_total_num_steps(cfg, train_dataset, update=True):
-    if not cfg.total_num_tokens and cfg.sample_packing:
+    if not cfg.total_num_tokens and not cfg.skip_dataset_preprocess:
         total_num_tokens = np.sum(
             train_dataset.data.column("input_ids")
             .to_pandas()
@@ -319,7 +319,11 @@ def calculate_total_num_steps(cfg, train_dataset, update=True):
 
     skip_estimates = cfg.model_config_type == "mamba"
 
-    if not skip_estimates and not cfg.total_supervised_tokens:
+    if (
+        not skip_estimates
+        and not cfg.total_supervised_tokens
+        and not cfg.skip_dataset_preprocess
+    ):
         total_supervised_tokens = (
             train_dataset.data.column("labels")
             .to_pandas()
