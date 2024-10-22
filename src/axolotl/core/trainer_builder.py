d1ed4a09c3209d6be28e410c00cea73011b09755
Wing Lian <wing.lian@gmail.com>
2024-10-22 08:40:58 -0400

    RewardDataCollatorWithPadding no longer has max_length

--- src/axolotl/core/trainer_builder.py
+++ src/axolotl/core/trainer_builder.py
@@ -1740,6 +1740,8 @@ class HFCausalTrainerBuilder(TrainerBuilderBase):
         ]
         if self.cfg.reward_model:
             collator = RewardDataCollatorWithPadding
+            if "max_length" in kwargs:
+                kwargs.pop("max_length")
         elif use_batch_sampler_collator:
             if self.cfg.model_config_type in SUPPORTED_MULTIPACK_MODEL_TYPES:
                 collator = V2BatchSamplerDataCollatorForSeq2Seq
