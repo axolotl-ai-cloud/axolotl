79b560b5a9b8d43b956f68101684c2cfc7ea8002
Mengqing Cao <cmq0113@163.com>
2024-10-25 21:06:56 +0800

    Refactor func load_model to class ModelLoader (#1909)

--- cicd/cicd.sh
+++ cicd/cicd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-pytest --ignore=tests/e2e/ /workspace/axolotl/tests/
+pytest -n4 --ignore=tests/e2e/ /workspace/axolotl/tests/
 pytest -n1 --dist loadfile -v /workspace/axolotl/tests/e2e/patched/ /workspace/axolotl/tests/e2e/integrations/
 pytest --ignore=tests/e2e/patched/ --ignore=tests/e2e/multigpu/ --ignore=tests/e2e/integrations/ /workspace/axolotl/tests/e2e/
