01a258efdee3e93fb66412f5cbfe720b861958dd
Wing Lian <wing.lian@gmail.com>
2024-11-12 17:58:26 -0500

    add P2P env when multi-gpu but not the full node (#2041)
    Co-authored-by: Wing Lian <wing@axolotl.ai>

---
+++ src/axolotl/utils/environment.py
@@ -0,0 +1,25 @@
+"""
+utils to get GPU info for the current environment
+"""
+from accelerate.utils.environment import (
+    check_cuda_p2p_ib_support as accelerate_check_cuda_p2p_ib_support,
+)
+from accelerate.utils.environment import get_gpu_info
+
+
+def check_cuda_p2p_ib_support():
+    if not accelerate_check_cuda_p2p_ib_support():
+        return False
+    unsupported_devices = {"RTX 6000 Ada"}
+    try:
+        device_names, device_count = get_gpu_info()
+        if 1 < device_count < 8:
+            if any(
+                device_name in unsupported_device
+                for device_name in device_names
+                for unsupported_device in unsupported_devices
+            ):
+                return False
+    except Exception:  # pylint: disable=broad-except # nosec
+        pass
+    return True
