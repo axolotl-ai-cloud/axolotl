8e8a48fb741d8883acc21b0a1b17f8dcfe58975f
sunny <sunnyliu19981005@gmail.com>
2024-11-13 13:05:37 -0500

    use version.parse

--- tests/e2e/utils.py
+++ tests/e2e/utils.py
@@ -6,10 +6,12 @@ import shutil
 import tempfile
 import unittest
 from functools import wraps
-from importlib.metadata import version
 from pathlib import Path
 
 import torch
+
+# from importlib.metadata import version
+from packaging import version
 
 
 def with_temp_dir(test_func):
@@ -43,8 +45,8 @@ def require_torch_2_3_1(test_case):
     """
 
     def is_min_2_3_1():
-        torch_version = version("torch")
-        return torch_version >= "2.3.1"
+        torch_version = version.parse(torch.__version__)
+        return torch_version >= version.parse("2.3.1")
 
     return unittest.skipUnless(is_min_2_3_1(), "test torch 2.3.1")(test_case)
 
@@ -55,8 +57,8 @@ def require_torch_2_5_1(test_case):
     """
 
     def is_min_2_5_1():
-        torch_version = version("torch")
-        return torch_version >= "2.5.1"
+        torch_version = version.parse(torch.__version__)
+        return torch_version >= version.parse("2.5.1")
 
     return unittest.skipUnless(is_min_2_5_1(), "test torch 2.5.1")(test_case)
 
