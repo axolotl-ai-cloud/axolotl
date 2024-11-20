035a504406ae421567f37f0f2a70a31b4508c6f9
Wing Lian <wing@axolotl.ai>
2024-11-20 14:21:56 -0500

    fix ampere check exception for ci

--- scripts/unsloth_install.py
+++ scripts/unsloth_install.py
@@ -8,7 +8,10 @@ from packaging.version import Version as V
 
 v = V(torch.__version__)
 cuda = str(torch.version.cuda)
-is_ampere = torch.cuda.get_device_capability()[0] >= 8
+try:
+    is_ampere = torch.cuda.get_device_capability()[0] >= 8
+except RuntimeError:
+    is_ampere = False
 if cuda != "12.1" and cuda != "11.8" and cuda != "12.4":
     raise RuntimeError(f"CUDA = {cuda} not supported!")
 if v <= V("2.1.0"):
