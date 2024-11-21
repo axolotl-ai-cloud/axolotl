8cbf4c7001ce9f9d35f8b6afd11fd220d88bb074
bursteratom <sunny19981005@outlook.com>
2024-11-21 14:02:59 -0500

    skip ds tagging if RewardTrainer

--- src/axolotl/core/trainer_builder.py
+++ src/axolotl/core/trainer_builder.py
@@ -1786,9 +1786,7 @@ class HFCausalTrainerBuilder(TrainerBuilderBase):
         else:
             trainer_kwargs["tokenizer"] = self.tokenizer
 
-        if (
-            not isinstance(trainer_cls, AxolotlRewardTrainer)
-        ) and self.cfg.datasets is not None:
+        if (not trainer_cls == AxolotlRewardTrainer) and self.cfg.datasets is not None:
             trainer_kwargs["dataset_tag_names"] = [
                 d["path"] for d in self.cfg.datasets if not Path(d["path"]).is_dir()
             ]
