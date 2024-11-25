f396d4f74b27e80b2446f5972588e2ea365488bd
bursteratom <sunny19981005@outlook.com>
2024-11-25 10:30:12 -0500

    added torch check for adopt, wip

--- src/axolotl/utils/__init__.py
+++ src/axolotl/utils/__init__.py
@@ -3,6 +3,9 @@ Basic utils for Axolotl
 """
 import importlib.util
 
+import torch
+from packaging import version
+
 
 def is_mlflow_available():
     return importlib.util.find_spec("mlflow") is not None
@@ -10,3 +13,8 @@ def is_mlflow_available():
 
 def is_comet_available():
     return importlib.util.find_spec("comet_ml") is not None
+
+
+def is_torch_min(version_string: str):
+    torch_version = version.parse(torch.__version__)
+    return torch_version >= version.parse(version_string)
