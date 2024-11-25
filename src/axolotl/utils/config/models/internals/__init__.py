3e8ee25646fa9dc74e4ab26d18335722518590f4
bursteratom <sunny19981005@outlook.com>
2024-11-25 16:22:03 -0500

    figuring out how pydantic works

--- src/axolotl/utils/config/models/internals/__init__.py
+++ src/axolotl/utils/config/models/internals/__init__.py
@@ -17,4 +17,4 @@ class GPUCapabilities(BaseModel):
 class ENVCapabilities(BaseModel):
     """model to manage the environment capabilities statically"""
 
-    torch_version: str = Field(default=None)
+    torch_version: Optional[str] = Field(default=None)
