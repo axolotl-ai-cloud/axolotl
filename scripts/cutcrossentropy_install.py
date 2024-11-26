93a0f1a6ef066f50d35cf374c5bf0f85b36661e6
NanoCode012 <nano@axolotl.ai>
2024-11-26 15:16:52 +0700

    feat: add install to CI

---
+++ scripts/cutcrossentropy_install.py
@@ -0,0 +1,16 @@
+"""Script to output the correct installation command for cut-cross-entropy."""
+
+try:
+    import torch
+except ImportError as exc:
+    raise ImportError("Install torch via `pip install torch`") from exc
+from packaging.version import Version as V
+
+v = V(torch.__version__)
+
+if v < V("2.4.0"):
+    raise RuntimeError(f"Torch = {v} too old!")
+
+print(
+    'pip install "cut-cross-entropy @ git+https://github.com/apple/ml-cross-entropy.git@9c297c905f55b73594b5d650722d1e78183b77bd"'
+)
