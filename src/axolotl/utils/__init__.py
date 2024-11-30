1e4af580e26cee7c181f75eb94a69c1b39dc3737
bursteratom <sunny19981005@outlook.com>
2024-11-30 12:36:17 -0500

    removed unused is_totch_min

--- src/axolotl/utils/__init__.py
+++ src/axolotl/utils/__init__.py
@@ -3,9 +3,6 @@ Basic utils for Axolotl
 """
 import importlib.util
 
-import torch
-from packaging import version
-
 
 def is_mlflow_available():
     return importlib.util.find_spec("mlflow") is not None
@@ -13,8 +10,3 @@ def is_mlflow_available():
 
 def is_comet_available():
     return importlib.util.find_spec("comet_ml") is not None
-
-
-def is_torch_min(version_string: str):
-    torch_version = version.parse(torch.__version__)
-    return torch_version >= version.parse(version_string)
