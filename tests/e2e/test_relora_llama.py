f4cb178d84278eb26a458bc2a64acfd1107cd343
Wing Lian <wing@axolotl.ai>
2024-12-02 21:52:09 -0500

    fix relora test to check grad_norm

--- tests/e2e/test_relora_llama.py
+++ tests/e2e/test_relora_llama.py
@@ -7,13 +7,15 @@ import os
 import unittest
 from pathlib import Path
 
+from tbparse import SummaryReader
+
 from axolotl.cli import load_datasets
 from axolotl.common.cli import TrainerCliArgs
 from axolotl.train import train
 from axolotl.utils.config import normalize_config
 from axolotl.utils.dict import DictDefault
 
-from .utils import with_temp_dir
+from .utils import most_recent_subdir, with_temp_dir
 
 LOG = logging.getLogger("axolotl.tests.e2e")
 os.environ["WANDB_DISABLED"] = "true"
@@ -29,36 +31,41 @@ class TestReLoraLlama(unittest.TestCase):
         # pylint: disable=duplicate-code
         cfg = DictDefault(
             {
-                "base_model": "JackFram/llama-68m",
-                "tokenizer_type": "LlamaTokenizer",
-                "sequence_len": 1024,
+                "base_model": "HuggingFaceTB/SmolLM2-135M",
+                "sequence_len": 2048,
                 "load_in_8bit": True,
                 "adapter": "lora",
-                "lora_r": 32,
+                "lora_r": 8,
                 "lora_alpha": 16,
                 "lora_dropout": 0.05,
                 "lora_target_modules": ["q_proj", "v_proj"],
-                "relora_steps": 25,
-                "relora_warmup_steps": 5,
+                "relora_steps": 50,
+                "relora_warmup_steps": 10,
                 "relora_anneal_steps": 5,
+                "relora_prune_ratio": 0.9,
                 "relora_cpu_offload": True,
                 "val_set_size": 0.0,
-                "special_tokens": {},
+                "special_tokens": {
+                    "pad_token": "<|endoftext|>",
+                },
+                "chat_template": "chatml",
                 "datasets": [
                     {
                         "path": "mhenrichsen/alpaca_2k_test",
                         "type": "alpaca",
                     },
                 ],
-                "warmup_steps": 15,
+                "warmup_steps": 10,
                 "num_epochs": 2,
-                "max_steps": 51,  # at least 2x relora_steps
-                "micro_batch_size": 4,
+                "max_steps": 105,  # at least 2x relora_steps
+                "micro_batch_size": 2,
                 "gradient_accumulation_steps": 1,
                 "output_dir": temp_dir,
                 "learning_rate": 0.00001,
-                "optimizer": "adamw_torch",
+                "optimizer": "adamw_8bit",
                 "lr_scheduler": "cosine",
+                "save_safetensors": True,
+                "use_tensorboard": True,
             }
         )
         normalize_config(cfg)
@@ -66,4 +73,14 @@ class TestReLoraLlama(unittest.TestCase):
         dataset_meta = load_datasets(cfg=cfg, cli_args=cli_args)
 
         train(cfg=cfg, cli_args=cli_args, dataset_meta=dataset_meta)
-        assert (Path(temp_dir) / "model.safetensors").exists()
+        assert (
+            Path(temp_dir) / "checkpoint-100/adapter/adapter_model.safetensors"
+        ).exists()
+        assert (Path(temp_dir) / "checkpoint-100/relora/model.safetensors").exists()
+
+        tb_log_path = most_recent_subdir(temp_dir + "/runs")
+        event_file = os.path.join(tb_log_path, sorted(os.listdir(tb_log_path))[0])
+        reader = SummaryReader(event_file)
+        df = reader.scalars  # pylint: disable=invalid-name
+        df = df[(df.tag == "train/grad_norm")]  # pylint: disable=invalid-name
+        assert df.value.values[-1] < 0.1, "Loss is too high"
