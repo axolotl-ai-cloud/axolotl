4ae4ea03d2bf7cbdd0132f0db00a92f98ddeff08
bursteratom <sunny19981005@outlook.com>
2024-12-03 12:05:30 -0500

    removed test file

--- test_hymba.py
+++
@@ -1,9 +0,0 @@
-# pylint: skip-file
-
-from transformers import AutoModelForCausalLM
-
-model = AutoModelForCausalLM.from_pretrained(
-    "nvidia/Hymba-1.5B-Base", trust_remote_code=True
-)
-
-repr(model.config)
