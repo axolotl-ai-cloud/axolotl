9f6d0b558781528441d0c36a46d54b25183245ee
Wing Lian <wing.lian@gmail.com>
2024-12-02 20:14:40 -0500

    use pytest sugar and verbose for more info during ci (#2112) [skip ci]
    * use pytest sugar and verbose for more info during ci
    
    * also run test suite when test requirements or cicd.sh changes
    
    * also on PR too

--- cicd/cicd.sh
+++ cicd/cicd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-pytest --durations=10 -n8 --ignore=tests/e2e/ /workspace/axolotl/tests/
-pytest --durations=10 -n1 --dist loadfile -v /workspace/axolotl/tests/e2e/patched/ /workspace/axolotl/tests/e2e/integrations/
-pytest --durations=10 --ignore=tests/e2e/patched/ --ignore=tests/e2e/multigpu/ --ignore=tests/e2e/integrations/ /workspace/axolotl/tests/e2e/
+pytest -v --durations=10 -n8 --ignore=tests/e2e/ /workspace/axolotl/tests/
+pytest -v --durations=10 -n1 --dist loadfile -v /workspace/axolotl/tests/e2e/patched/ /workspace/axolotl/tests/e2e/integrations/
+pytest -v --durations=10 --ignore=tests/e2e/patched/ --ignore=tests/e2e/multigpu/ --ignore=tests/e2e/integrations/ /workspace/axolotl/tests/e2e/
