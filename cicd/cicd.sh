d72dc385780418b183b1cd4005546a25bada5037
Wing Lian <wing@axolotl.ai>
2024-12-10 12:04:39 -0500

    add checks for correct pytorch version so this doesn't slip by again

--- cicd/cicd.sh
+++ cicd/cicd.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+python -c "import torch; assert '$PYTORCH_VERSION' in torch.__version__"
+
 pytest -v --durations=10 -n8 --ignore=tests/e2e/ --ignore=tests/patched/ /workspace/axolotl/tests/
 # pytest -v --durations=10 -n8 --dist loadfile /workspace/axolotl/tests/patched/
 pytest -v --durations=10 -n1 --dist loadfile /workspace/axolotl/tests/e2e/patched/
