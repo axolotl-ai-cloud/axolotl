573b8cae48e0b00a5cf12817adc55f1c5fe6e1c5
Wing Lian <wing@axolotl.ai>
2025-01-09 01:19:06 -0500

    fix the tests and imports

--- tests/integrations/test_liger.py
+++ tests/integrations/test_liger.py
@@ -7,11 +7,11 @@ from typing import Optional
 
 import pytest
 
-from axolotl.utils.config import validate_config
+from axolotl.utils.config import prepare_plugins, validate_config
 from axolotl.utils.dict import DictDefault
 
 
-@pytest.fixture(name="minimal_base_cfg")
+@pytest.fixture(name="minimal_liger_cfg")
 def fixture_cfg():
     return DictDefault(
         {
@@ -25,6 +25,7 @@ def fixture_cfg():
             ],
             "micro_batch_size": 1,
             "gradient_accumulation_steps": 1,
+            "plugins": ["axolotl.integrations.liger.LigerPlugin"],
         }
     )
 
@@ -47,34 +48,36 @@ class TestValidation(BaseValidation):
     Test the validation module for liger
     """
 
-    def test_deprecated_swiglu(self, minimal_base_cfg):
+    def test_deprecated_swiglu(self, minimal_liger_cfg):
         test_cfg = DictDefault(
             {
                 "liger_swiglu": False,
             }
-            | minimal_base_cfg
+            | minimal_liger_cfg
         )
 
         with self._caplog.at_level(logging.WARNING):
+            prepare_plugins(test_cfg)
             updated_cfg = validate_config(test_cfg)
             assert (
                 "The 'liger_swiglu' argument is deprecated"
                 in self._caplog.records[0].message
             )
             assert updated_cfg.liger_swiglu is None
-            assert updated_cfg.liger_glu_activations is False
+            assert updated_cfg.liger_glu_activation is False
 
-    def test_conflict_swiglu_ligergluactivation(self, minimal_base_cfg):
+    def test_conflict_swiglu_ligergluactivation(self, minimal_liger_cfg):
         test_cfg = DictDefault(
             {
                 "liger_swiglu": False,
-                "liger_glu_activations": True,
+                "liger_glu_activation": True,
             }
-            | minimal_base_cfg
+            | minimal_liger_cfg
         )
 
         with pytest.raises(
             ValueError,
             match=r".*You cannot have both `liger_swiglu` and `liger_glu_activation` set.*",
         ):
+            prepare_plugins(test_cfg)
             validate_config(test_cfg)
