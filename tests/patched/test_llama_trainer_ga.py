9bd12c66b949e43bd4309f6d61423476834eb5d7
Wing Lian <wing@axolotl.ai>
2025-01-13 10:34:44 -0500

    bump to latest transformers release

--- tests/patched/test_llama_trainer_ga.py
+++ tests/patched/test_llama_trainer_ga.py
@@ -1,12 +1,15 @@
 """"Test module for checking whether the Hugging Face Transformers is working as expected."""
 import unittest
 
+import pytest
+
 from axolotl.monkeypatch.trainer_grad_accum import (
     check_forward_is_patchable,
     check_training_step_is_patchable,
 )
 
 
+@pytest.mark.skip("may not be needed for latest transformers version")
 class TestTrainerGAIntegration(unittest.TestCase):
     """llama monkeypatch integration tests."""
 
