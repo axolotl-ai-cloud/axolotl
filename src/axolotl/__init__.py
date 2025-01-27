4d1553e53f31af952928fd1ec68f17c6a7252276
Dan Saunders <dan@axolotl.ai>
2025-01-27 15:43:51 -0500

    updates

--- src/axolotl/__init__.py
+++ src/axolotl/__init__.py
@@ -2,6 +2,20 @@
 
 import pkgutil
 
-__path__ = pkgutil.extend_path(__path__, __name__)  # Make this a namespace package
+from .cli.config import choose_config, load_cfg, validate_config
+from .datasets import ConstantLengthDataset, TokenizedPromptDataset
+from .evaluate import evaluate
+from .train import train
 
+__path__ = pkgutil.extend_path(__path__, __name__)  # Make this a namespace package
 __version__ = "0.6.0"
+
+__all__ = [
+    "train",
+    "evaluate",
+    "TokenizedPromptDataset",
+    "ConstantLengthDataset",
+    "load_cfg",
+    "choose_config",
+    "validate_config",
+]
