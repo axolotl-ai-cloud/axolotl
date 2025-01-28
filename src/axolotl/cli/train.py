7622c2d008d0ea06ae98bb93e41038eb56b3db47
Wing Lian <wing.lian@gmail.com>
2025-01-28 12:52:55 -0500

    use output_dir instead of hardcoded saves path
    Co-authored-by: NanoCode012 <kevinvong@rocketmail.com>

--- src/axolotl/cli/train.py
+++ src/axolotl/cli/train.py
@@ -80,7 +80,7 @@ def do_cli(config: Union[Path, str] = Path("examples/"), **kwargs) -> None:
             ),
             run_config=RunConfig(
                 name=parsed_cfg.ray_run_name,
-                storage_path=Path("./saves").absolute().as_posix(),
+                storage_path=Path(cfg.output_dir).absolute().as_posix(),
             ),
         )
         return trainer.fit()
