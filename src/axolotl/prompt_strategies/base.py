79159b4871c6aae6467cdeb183fe88f4c2d9a0bf
Wing Lian <wing@axolotl.ai>
2025-02-02 23:47:00 -0500

    support custom module prompt strategy for rl

--- src/axolotl/prompt_strategies/base.py
+++ src/axolotl/prompt_strategies/base.py
@@ -14,6 +14,16 @@ def load(strategy, cfg, module_base=None, **kwargs):
             strategy = strategy + ".default"
         load_fn = strategy.split(".")[-1]
         strategy = ".".join(strategy.split(".")[:-1])
+        if len(strategy.split(".")) > 1:
+            try:
+                importlib.import_module(
+                    "." + strategy.split(".")[-1],
+                    ".".join(strategy.split(".")[:-1]),
+                )
+                module_base = ".".join(strategy.split(".")[:-1])
+                strategy = strategy.split(".")[-1]
+            except ModuleNotFoundError:
+                pass
         mod = importlib.import_module(f".{strategy}", module_base)
         func = getattr(mod, load_fn)
         return func(cfg, **kwargs)
