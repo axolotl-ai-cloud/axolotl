d663652216e23b38138d041c0d5c0fdb12ded91d
Sunny Liu <sunny19981005@outlook.com>
2025-02-18 17:08:32 -0500

    del device_map for tp

--- src/axolotl/utils/models.py
+++ src/axolotl/utils/models.py
@@ -764,6 +764,7 @@ class ModelLoader:
         skip_move_to_device = False
         if self.cfg.tp_size is not None:
             self.model_kwargs["tp_plan"] = "auto"
+            del self.model_kwargs["device_map"]
 
         if (  # pylint: disable=condition-evals-to-constant)
             (self.cfg.fsdp and self.cfg.fsdp_config.fsdp_cpu_ram_efficient_loading)
