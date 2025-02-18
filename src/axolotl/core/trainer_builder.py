952ce7eacc7244da9e5394e5d051c90499254541
Sunny Liu <sunny19981005@outlook.com>
2025-02-18 16:20:24 -0500

    enabe tp thru tp_size

--- src/axolotl/core/trainer_builder.py
+++ src/axolotl/core/trainer_builder.py
@@ -703,6 +703,9 @@ class HFCausalTrainerBuilder(TrainerBuilderBase):
                 "accelerator_config"
             ] = self.cfg.accelerator_config
 
+        if self.cfg.tp_size is not None:
+            training_arguments_kwargs["tp_size"] = self.cfg.tp_size
+
         if self.cfg.kd_ce_alpha is not None:
             training_arguments_kwargs["kd_ce_alpha"] = self.cfg.kd_ce_alpha
         if self.cfg.kd_alpha is not None:
