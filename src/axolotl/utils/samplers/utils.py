90dfcd8c0315a7b8d52578e79f77e378ea395486
Sung Ching Liu <22844540+bursteratom@users.noreply.github.com>
2025-02-19 21:13:25 -0500

    Revert "Fix sample packing producing longer sequences than specified by `sequ…"
    This reverts commit 8dfadc2b3c7f85e4208b74eb93c32f440c7e25b4.

--- src/axolotl/utils/samplers/utils.py
+++ src/axolotl/utils/samplers/utils.py
@@ -5,12 +5,12 @@ import numpy as np
 
 
 def get_dataset_lengths(dataset):
-    if "length" in dataset.column_names:
-        lengths = np.array(dataset["length"])
-    elif "position_ids" in dataset.column_names:
-        position_ids = dataset["position_ids"]
+    if "length" in dataset.data.column_names:
+        lengths = np.array(dataset.data.column("length"))
+    elif "position_ids" in dataset.data.column_names:
+        position_ids = dataset.data.column("position_ids")
         lengths = np.array([x[-1] + 1 for x in position_ids])
     else:
-        input_ids = dataset["input_ids"]
-        lengths = np.array([len(seq) for seq in input_ids])
+        input_ids = dataset.data.column("input_ids")
+        lengths = np.vectorize(len)(np.array(input_ids, dtype=object))
     return lengths
