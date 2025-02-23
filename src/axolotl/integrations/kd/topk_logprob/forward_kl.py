2d5826f544a05bc5d909c47915cc853d24ea61ab
Wing Lian <wing.lian@gmail.com>
2025-02-23 12:31:35 -0500

    Relicense the logprob KD loss functions as Apache 2.0 (#2358)

--- src/axolotl/integrations/kd/topk_logprob/forward_kl.py
+++ src/axolotl/integrations/kd/topk_logprob/forward_kl.py
@@ -1,14 +1,16 @@
 # Copyright 2024 Axolotl AI. All rights reserved.
 #
-# This software may be used and distributed according to
-# the terms of the Axolotl Community License Agreement (the "License");
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations under
-# the License.
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """
 loss for top_k KL divergence
