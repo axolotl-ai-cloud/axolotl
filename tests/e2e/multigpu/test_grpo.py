e74179478fada84e1bff361b8c2cea45ab8eb30e
Wing Lian <wing@axolotl.ai>
2025-03-26 12:07:50 -0400

    grpo + vllm test on separate devices for now

--- tests/e2e/multigpu/test_grpo.py
+++ tests/e2e/multigpu/test_grpo.py
@@ -45,7 +45,8 @@ def oai_gsm8k_transform(cfg, *args, **kwargs):
 
     @pytest.mark.parametrize(
         "num_gpus",
-        [1, 2],
+        # [1, 2],
+        [1],
     )
     @require_vllm
     def test_llama_dora(self, temp_dir, num_gpus):
@@ -132,7 +133,8 @@ def oai_gsm8k_transform(cfg, *args, **kwargs):
 
     @pytest.mark.parametrize(
         "num_gpus",
-        [1, 2],
+        # [1, 2],
+        [1],
     )
     @require_vllm
     def test_llama_fft(self, temp_dir, num_gpus):
