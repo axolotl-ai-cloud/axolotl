b76d2d1130aa6a7d9846ebeab6c3f68405d51172
Rahul Tuli <rahul@neuralmagic.com>
2025-03-26 21:57:00 +0000

    Update: review comments!

--- src/axolotl/integrations/llmcompressor_sft/args.py
+++ src/axolotl/integrations/llmcompressor_sft/args.py
@@ -1,13 +1,35 @@
 """
-Pydantic model for accepting `llmcompressor` specific arguments.
+LLMCompressor and Sparse Finetuning config models.
 """
-from typing import Optional, Any
-from pydantic import BaseModel
+
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Any
+from typing_extensions import Annotated
+
+class SFTArgs(BaseModel):
+    """Sparse Finetuning config for LLMCompressor."""
+
+    # Typing for recipe is set to Any due to:
+    # https://github.com/vllm-project/llm-compressor/issues/1319
+    recipe: Annotated[
+        Any,
+        Field(description="Recipe config.")
+    ]
+
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True,
+        validate_assignment=True,
+    )
 
 
 class LLMCompressorArgs(BaseModel):
-    """
-    Input arguments for Sparse Finetuning.
-    """
-    
-    recipe: Optional[Any] = None
+    """LLMCompressor configuration BaseModel."""
+
+    llmcompressor: Annotated[
+        SFTArgs,
+        Field(description="SFT llmcompressor args")
+    ]
+
+    model_config = ConfigDict(
+        validate_assignment=True,
+    )
