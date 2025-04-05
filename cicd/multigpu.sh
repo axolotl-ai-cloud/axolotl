81378c2d7ea2cd64ada5a88cd545e609644050af
Wing Lian <wing@axolotl.ai>
2025-04-05 07:57:08 -0400

    get info on slow tests

--- cicd/multigpu.sh
+++ cicd/multigpu.sh
@@ -2,5 +2,5 @@
 set -e
 
 # only run one test at a time so as not to OOM the GPU
-pytest -v -n2 /workspace/axolotl/tests/e2e/multigpu/ --ignore=/workspace/axolotl/tests/e2e/multigpu/solo/
-pytest -v -n1 /workspace/axolotl/tests/e2e/multigpu/solo/
+pytest -v  --durations=10 -n2 /workspace/axolotl/tests/e2e/multigpu/ --ignore=/workspace/axolotl/tests/e2e/multigpu/solo/
+pytest -v  --durations=10 -n1 /workspace/axolotl/tests/e2e/multigpu/solo/
