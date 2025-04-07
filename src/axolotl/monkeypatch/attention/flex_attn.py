75c565d476aa381f6627120b60ca7a1860cfc0de
Sunny Liu <sunny19981005@outlook.com>
2025-04-07 17:06:51 -0400

    add back dynamic=False

--- src/axolotl/monkeypatch/attention/flex_attn.py
+++ src/axolotl/monkeypatch/attention/flex_attn.py
@@ -40,6 +40,7 @@ def patch_flex_wrapper():
             if not self._is_flex_compiled:
                 self._compiled_flex_attention = torch.compile(
                     flex_attention,
+                    dynamic=False,
                     mode="max-autotune-no-cudagraphs",
                     fullgraph=True,
                 )
