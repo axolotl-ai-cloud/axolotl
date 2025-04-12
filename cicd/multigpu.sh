35a0d28605fb39b0f45b8ac7951400cffad6b9b7
Dan Saunders <dan@axolotl.ai>
2025-04-11 22:30:56 -0400

    fix

--- cicd/multigpu.sh
+++ cicd/multigpu.sh
@@ -4,7 +4,7 @@ set -e
 # only run one test at a time so as not to OOM the GPU
 pytest -v  --durations=10 -n2 /workspace/axolotl/tests/e2e/multigpu/ --ignore=/workspace/axolotl/tests/e2e/multigpu/solo/
 pytest -v  --durations=10 -n1 /workspace/axolotl/tests/e2e/multigpu/solo/
-=======
+
 # Only run two tests at a time to avoid OOM on GPU (with coverage collection)
 pytest -v -n2 \
   --ignore=/workspace/axolotl/tests/e2e/multigpu/solo/
