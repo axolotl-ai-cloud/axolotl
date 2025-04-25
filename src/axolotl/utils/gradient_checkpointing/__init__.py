2c2563bc341ab8896322011d04a32b81ec1daa41
Eko Julianto Salim <eko@ekojs.com>
2025-04-26 04:02:37 +0700

    fix: gradient checkpointing functools.partial object has no attribute __self__ (#2563) [skip ci]
    * fix: gradient checkpointing causing functools.partial error
    
    * lint
    
    * chore: lint
    
    ---------
    
    Co-authored-by: Wing Lian <wing@axolotl.ai>

--- src/axolotl/utils/gradient_checkpointing/__init__.py
+++ src/axolotl/utils/gradient_checkpointing/__init__.py
@@ -1,5 +1,7 @@
 """custom checkpointing utils"""
 
+from functools import partial
+
 from axolotl.utils.gradient_checkpointing.unsloth import (
     Unsloth_Offloaded_Gradient_Checkpointer,
 )
@@ -9,6 +11,10 @@ def hf_grad_checkpoint_offload_wrapper(
     decoder_layer, *args, use_reentrant=None
 ):  # pylint: disable=unused-argument
     return Unsloth_Offloaded_Gradient_Checkpointer.apply(
-        decoder_layer.__self__,
+        (
+            decoder_layer.func.__self__
+            if isinstance(decoder_layer, partial)
+            else decoder_layer.__self__
+        ),
         *args,
     )
