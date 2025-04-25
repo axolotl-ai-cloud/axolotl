0e7b1a5254b81f02a844536d0f89b87bb564aa5b
Wing Lian <wing@axolotl.ai>
2025-04-25 09:50:43 -0400

    chore: lint

--- src/axolotl/utils/gradient_checkpointing/__init__.py
+++ src/axolotl/utils/gradient_checkpointing/__init__.py
@@ -11,8 +11,10 @@ def hf_grad_checkpoint_offload_wrapper(
     decoder_layer, *args, use_reentrant=None
 ):  # pylint: disable=unused-argument
     return Unsloth_Offloaded_Gradient_Checkpointer.apply(
-        decoder_layer.func.__self__
-        if isinstance(decoder_layer, partial)
-        else decoder_layer.__self__,
+        (
+            decoder_layer.func.__self__
+            if isinstance(decoder_layer, partial)
+            else decoder_layer.__self__
+        ),
         *args,
     )
