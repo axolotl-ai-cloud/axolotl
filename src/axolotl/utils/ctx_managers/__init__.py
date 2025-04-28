d23e53e58a58e90f8a825b7c4b751eb04b457d89
Dan Saunders <danjsaund@gmail.com>
2025-04-28 17:31:20 +0000

    update

---
+++ src/axolotl/utils/ctx_managers/__init__.py
@@ -0,0 +1,7 @@
+"""Init for context manager submodule"""
+
+# pylint: disable=unused-import
+# flake8: noqa
+
+from .sequence_parallel import SequenceParallelContextManager
+from .dtensor_sequence_parallel import DTensorSequenceParallelContextManager
