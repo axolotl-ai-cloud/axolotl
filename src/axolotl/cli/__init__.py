7b52126138d0e96a745e7a87bf4cdc02636965b6
Wing Lian <wing@axolotl.ai>
2025-05-01 09:34:02 -0400

    only configure logging on cli to play nicely with colab

--- src/axolotl/cli/__init__.py
+++ src/axolotl/cli/__init__.py
@@ -2,4 +2,7 @@
 
 import os
 
+from axolotl.logging_config import configure_logging
+
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+configure_logging()
--- src/axolotl/train.py
+++ src/axolotl/train.py
@@ -30,7 +30,6 @@ from axolotl.core.trainers.mixins.sequence_parallel import (
     SequenceParallelContextManager,
 )
 from axolotl.integrations.base import PluginManager
-from axolotl.logging_config import configure_logging
 from axolotl.utils.dict import DictDefault
 from axolotl.utils.distributed import cleanup_distributed
 from axolotl.utils.freeze import freeze_layers_except
@@ -42,7 +41,6 @@ try:
 except ImportError:
     BetterTransformer = None
 
-configure_logging()
 LOG = get_logger(__name__)
 
 
