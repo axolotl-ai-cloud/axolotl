2b1cfb24591c2930a786da4e4fb0deee547eb6ca
Dan Saunders <danjsaund@gmail.com>
2025-05-22 18:41:25 +0000

    progress

--- src/axolotl/utils/data/__init__.py
+++ src/axolotl/utils/data/__init__.py
@@ -7,8 +7,6 @@ from axolotl.utils.data.pretraining import (
 from axolotl.utils.data.rl import load_prepare_preference_datasets
 from axolotl.utils.data.sft import (
     get_dataset_wrapper,
-    load_prepare_datasets,
-    load_tokenized_prepared_datasets,
     prepare_dataset,
 )
 from axolotl.utils.data.utils import md5
@@ -18,8 +16,6 @@ __all__ = [
     "wrap_pretraining_dataset",
     "load_prepare_preference_datasets",
     "get_dataset_wrapper",
-    "load_prepare_datasets",
-    "load_tokenized_prepared_datasets",
     "prepare_dataset",
     "md5",
 ]
