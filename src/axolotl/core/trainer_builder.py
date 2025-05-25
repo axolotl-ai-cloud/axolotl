a67a7ce24420e978a0c28bc3f763eb71878b9a04
Wing Lian <wing@axolotl.ai>
2025-05-25 16:53:20 -0400

    add close to comment block

--- src/axolotl/core/trainer_builder.py
+++ src/axolotl/core/trainer_builder.py
@@ -305,6 +305,7 @@ class HFCausalTrainerBuilder(TrainerBuilderBase):
     def _get_trainer_cls(self):
         """
         Gets the trainer class for the given configuration.
+        """
         if self.cfg.plugins:
             plugin_manager = PluginManager.get_instance()
             trainer_cls = plugin_manager.get_trainer_cls(self.cfg)
