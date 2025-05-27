49369db40a911c9a3e52a46d0b097da9a91895ca
NanoCode012 <nano@axolotl.ai>
2025-05-27 18:11:24 +0700

    feat: change logger class to AxolotlLogger to filter non-axolotl infos or below

--- src/axolotl/logging_config.py
+++ src/axolotl/logging_config.py
@@ -2,13 +2,40 @@
 Common logging module for axolotl
 """
 
+import logging
 import os
 import sys
-from logging import Formatter
+from logging import Formatter, Logger, LogRecord
 from logging.config import dictConfig
 from typing import Any, Dict
 
 from colorama import Fore, Style, init
+
+
+class AxolotlOrWarnErrorFilter(logging.Filter):
+    """
+    Allows ANY WARNING+ or ERROR+
+    Allows axolotl.* at INFO or higher
+    Drops all other records (i.e. non-axolotl.INFO, DEBUG, etc.)
+    """
+
+    def filter(self, record: LogRecord) -> bool:
+        # allow WARNING+ or ERROR+ from anywhere
+        if record.levelno >= logging.WARNING:
+            return True
+
+        # else allow axolotl.* at INFO or higher
+        return record.name.startswith("axolotl") and record.levelno >= logging.INFO
+
+
+class AxolotlLogger(Logger):
+    """A Logger that automatically rejects non-axolotl INFOs."""
+
+    def __init__(self, name: str, level: int = logging.NOTSET):
+        super().__init__(name, level)
+
+        # set global filter on the logger itself
+        self.addFilter(AxolotlOrWarnErrorFilter())
 
 
 class ColorfulFormatter(Formatter):
@@ -30,7 +57,7 @@ class ColorfulFormatter(Formatter):
 
 DEFAULT_LOGGING_CONFIG: Dict[str, Any] = {
     "version": 1,
-    "disable_existing_loggers": False,
+    "disable_existing_loggers": True,
     "formatters": {
         "simple": {
             "format": "[%(asctime)s] [%(levelname)s] [%(name)s.%(funcName)s:%(lineno)d] [PID:%(process)d] %(message)s",
@@ -55,6 +82,7 @@ DEFAULT_LOGGING_CONFIG: Dict[str, Any] = {
             "stream": sys.stdout,
         },
     },
+    # log level will be superseded by the AxolotlLogger
     "root": {"handlers": ["console"], "level": os.getenv("LOG_LEVEL", "WARNING")},
     "loggers": {
         "axolotl": {
@@ -70,3 +98,4 @@ def configure_logging():
     """Configure with default logging"""
     init()  # Initialize colorama
     dictConfig(DEFAULT_LOGGING_CONFIG)
+    logging.setLoggerClass(AxolotlLogger)
