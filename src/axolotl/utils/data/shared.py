b3db11030c2bfff6fd9c7f9d256f497d8414ebe5
NanoCode012 <nano@axolotl.ai>
2025-06-30 10:24:23 +0700

    fix: shuffle dataset even if only one to fix tokenize stall

--- src/axolotl/utils/data/shared.py
+++ src/axolotl/utils/data/shared.py
@@ -524,7 +524,13 @@ def merge_datasets(datasets: list[Dataset], cfg: DictDefault) -> Dataset:
         Merged dataset.
     """
     if len(datasets) == 1:
-        return datasets[0]
+        ds = datasets[0]
+
+        # Do not shuffle if curriculum sampling is enabled
+        if not cfg.curriculum_sampling:
+            return ds.shuffle(seed=cfg.seed)
+
+        return ds
 
     LOG.info("Merging datasets...")
     merged_dataset = concatenate_datasets(datasets)
