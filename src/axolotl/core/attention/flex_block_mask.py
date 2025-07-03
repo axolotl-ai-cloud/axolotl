fix causal mask patch

src/axolotl/core/attention/flex_block_mask.py
@@ -12,6 +12,7 @@ from transformers.masking_utils import (
     ALL_MASK_ATTENTION_FUNCTIONS,
     _preprocess_mask_arguments,
     and_masks,
+    causal_mask_function,
     or_masks,
 )
 from transformers.utils import is_torch_greater_or_equal
@@ -63,37 +64,45 @@ def create_causal_mask(
     else:
         layer_idx = 0
 
-    original_attention_mask = attention_mask.clone().to(cache_position.device)
+    original_attention_mask = (
+        None
+        if attention_mask is None
+        else attention_mask.clone().to(cache_position.device)
+    )
     early_exit, attention_mask, kv_length, kv_offset = _preprocess_mask_arguments(
         config, input_embeds, attention_mask, cache_position, past_key_values, layer_idx
     )
     if early_exit:
         return attention_mask
 
-    batch_size, total_seq_len = original_attention_mask.shape
+    batch_size, total_seq_len = cache_position.shape
     key_length = total_seq_len
     document_ids = torch.nn.functional.pad(
         original_attention_mask, value=0, pad=(0, key_length)
     )
 
-    def causal_doc_mask_mod(
-        batch_idx, head_idx, q_idx, kv_idx
-    ):  # pylint: disable=unused-argument
-        """
-        Defines the logic of a block causal mask by combining both a standard causal mask
-        and a block diagonal document mask.
-        See :func:`~torchtune.modules.attention_utils.create_block_causal_mask`
-        for an illustration.
-        """
-        causal_mask_ = q_idx >= kv_idx  # not valid when decoding
-        document_mask = (
-            document_ids[batch_idx, q_idx] == document_ids[batch_idx, kv_idx]
-        )
-        final_mask = causal_mask_ & document_mask
-        return final_mask
-
     batch_size, dtype = input_embeds.shape[0], input_embeds.dtype
-    mask_factory_function = causal_doc_mask_mod
+    if attention_mask is not None:
+
+        def causal_doc_mask_mod(
+            batch_idx, head_idx, q_idx, kv_idx
+        ):  # pylint: disable=unused-argument
+            """
+            Defines the logic of a block causal mask by combining both a standard causal mask
+            and a block diagonal document mask.
+            See :func:`~torchtune.modules.attention_utils.create_block_causal_mask`
+            for an illustration.
+            """
+            causal_mask_ = q_idx >= kv_idx  # not valid when decoding
+            document_mask = (
+                document_ids[batch_idx, q_idx] == document_ids[batch_idx, kv_idx]
+            )
+            final_mask = causal_mask_ & document_mask
+            return final_mask
+
+        mask_factory_function = causal_doc_mask_mod
+    else:
+        mask_factory_function = causal_mask_function
     mask_interface = ALL_MASK_ATTENTION_FUNCTIONS[
         config._attn_implementation  # pylint: disable=protected-access
     ]