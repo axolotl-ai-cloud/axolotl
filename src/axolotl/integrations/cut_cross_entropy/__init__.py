b7e8f66e5a78d775f5bb9e7f0aa2f2f6a2458b71
Wing Lian <wing@axolotl.ai>
2025-07-21 11:41:53 -0400

    upstream fixes in cce for dora and tensor paralel support (#2960) [skip ci]

--- src/axolotl/integrations/cut_cross_entropy/__init__.py
+++ src/axolotl/integrations/cut_cross_entropy/__init__.py
@@ -34,7 +34,7 @@ LOG = get_logger(__name__)
 
 _CCE_INSTALL_MESSAGE = (
     "Please install Axolotl's fork of cut_cross_entropy with transformers support using "
-    '`pip install "cut-cross-entropy[transformers] @ git+https://github.com/axolotl-ai-cloud/ml-cross-entropy.git@50cef19"`'
+    '`pip install "cut-cross-entropy[transformers] @ git+https://github.com/axolotl-ai-cloud/ml-cross-entropy.git@631d646"`'
 )
 
 
