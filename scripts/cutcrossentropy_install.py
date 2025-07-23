b1b8c3b3430d6aee2ce745dcf2dc6bd428b46d81
NanoCode012 <nano@axolotl.ai>
2025-07-23 17:36:25 +0700

    feat: update cce commit to include voxtral

--- scripts/cutcrossentropy_install.py
+++ scripts/cutcrossentropy_install.py
@@ -29,5 +29,5 @@ UV_PREFIX = "uv " if USE_UV else ""
 
 print(
     UNINSTALL_PREFIX
-    + f'{UV_PREFIX}pip install "cut-cross-entropy[transformers] @ git+https://github.com/axolotl-ai-cloud/ml-cross-entropy.git@631d646"'
+    + f'{UV_PREFIX}pip install "cut-cross-entropy[transformers] @ git+https://github.com/axolotl-ai-cloud/ml-cross-entropy.git@010c3ac3f1e725098961832830303eeb4142dd88"'
 )
