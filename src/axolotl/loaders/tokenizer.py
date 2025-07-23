406b64d60f2d1a1085cac721a51ae349bd59893b
NanoCode012 <nano@axolotl.ai>
2025-07-23 17:30:44 +0700

    feat: patch mistraltokenizer subclass upstream and add missing

--- src/axolotl/loaders/tokenizer.py
+++ src/axolotl/loaders/tokenizer.py
@@ -124,7 +124,12 @@ def load_tokenizer(cfg: DictDefault) -> PreTrainedTokenizer:
 
     def _load_mistral_common_tokenizer(cfg: DictDefault):
         """Load mistral-common tokenizer"""
+        from transformers import tokenization_mistral_common
+
         from axolotl.utils.mistral.mistral_tokenizer import HFMistralTokenizer
+
+        # patch
+        tokenization_mistral_common.MistralCommonTokenizer = HFMistralTokenizer
 
         # Load the HF-compatible wrapper around MistralTokenizer
         tokenizer = HFMistralTokenizer.from_pretrained(cfg.tokenizer_config)
