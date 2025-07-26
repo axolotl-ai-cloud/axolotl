ee945d02c2c1d8394053b9be3b5584752b1094a9
Wing Lian <wing@axolotl.ai>
2025-07-26 09:54:53 -0400

    cast to bool and debug out for now

--- src/axolotl/core/trainers/mixins/dist_parallel.py
+++ src/axolotl/core/trainers/mixins/dist_parallel.py
@@ -3,9 +3,10 @@
 import os
 
 import transformers.trainer
-from accelerate import PartialState
 from accelerate.utils import TorchTensorParallelPlugin
 from torch.distributed import DeviceMesh
+
+from axolotl.utils.environment import is_package_version_ge
 
 
 class DistParallelMixin(transformers.trainer.Trainer):
@@ -16,15 +17,19 @@ class DistParallelMixin(transformers.trainer.Trainer):
     def create_accelerator_and_postprocess(self):
         res = super().create_accelerator_and_postprocess()
 
-        if int(os.environ.get("WORLD_SIZE", "1")) > 1:
-            device_mesh: DeviceMesh = self.accelerator.state.device_mesh
-            mesh_dim_names: tuple[str, ...] | None = device_mesh.mesh_dim_names
-            if "tp" in mesh_dim_names and device_mesh["tp"].size() > 1:
-                self.accelerator.state.distributed_type = "TP"
-                PartialState().distributed_type = "TP"
-                tp_plugin = TorchTensorParallelPlugin(
-                    tp_size=device_mesh["tp"].size(), torch_device_mesh=device_mesh
-                )
-                self.accelerator.state.torch_tp_plugin = tp_plugin
+        if not is_package_version_ge("accelerate", "1.10.0"):
+            # pylint: disable=protected-access
+            if int(os.environ.get("WORLD_SIZE", "1")) > 1:
+                from accelerate.state import PartialState
+
+                device_mesh: DeviceMesh = PartialState()._shared_state["device_mesh"]
+                mesh_dim_names: tuple[str, ...] | None = device_mesh.mesh_dim_names
+                if "tp" in mesh_dim_names and device_mesh["tp"].size() > 1:
+                    self.accelerator.state.distributed_type = "TP"
+                    PartialState().distributed_type = "TP"
+                    tp_plugin = TorchTensorParallelPlugin(
+                        tp_size=device_mesh["tp"].size(), torch_device_mesh=device_mesh
+                    )
+                    self.accelerator.state.torch_tp_plugin = tp_plugin
 
         return res
--- src/axolotl/utils/environment.py
+++ src/axolotl/utils/environment.py
@@ -2,12 +2,15 @@
 utils to get GPU info for the current environment
 """
 
+from importlib.metadata import version
+
 from accelerate.utils.environment import (
     check_cuda_p2p_ib_support as accelerate_check_cuda_p2p_ib_support,
 )
 from accelerate.utils.environment import (
     get_gpu_info,
 )
+from packaging.version import Version, parse
 
 
 def check_cuda_p2p_ib_support():
@@ -26,3 +29,13 @@ def check_cuda_p2p_ib_support():
     except Exception:  # pylint: disable=broad-except # nosec
         pass
     return True
+
+
+def get_package_version(package: str) -> Version:
+    version_str = version(package)
+    return parse(version_str)
+
+
+def is_package_version_ge(package: str, version_: str) -> bool:
+    package_version = get_package_version(package)
+    return package_version >= parse(version_)
