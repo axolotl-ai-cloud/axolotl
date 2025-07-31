76bf20fb461f00c6fa6c2c88eb43377e9a52666b
NanoCode012 <nano@axolotl.ai>
2025-07-31 15:14:36 +0700

    feat: upgrade cce commit to include smollm3, granite, granitemoe

--- src/axolotl/integrations/cut_cross_entropy/__init__.py
+++ src/axolotl/integrations/cut_cross_entropy/__init__.py
@@ -34,7 +34,7 @@ LOG = get_logger(__name__)
 
 _CCE_INSTALL_MESSAGE = (
     "Please install Axolotl's fork of cut_cross_entropy with transformers support using "
-    '`pip install "cut-cross-entropy[transformers] @ git+https://github.com/axolotl-ai-cloud/ml-cross-entropy.git@010c3ac3f1e725098961832830303eeb4142dd88"`'
+    '`pip install "cut-cross-entropy[transformers] @ git+https://github.com/axolotl-ai-cloud/ml-cross-entropy.git@849c3c5"`'
 )
 
 
