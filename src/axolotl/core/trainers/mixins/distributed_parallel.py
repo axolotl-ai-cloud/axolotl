ab450f7e34f89287cfaff7fe06d72b5e61e07f02
Wing Lian <wing@axolotl.ai>
2025-08-07 14:30:24 -0400

    remove problematic barrier

--- src/axolotl/core/trainers/mixins/distributed_parallel.py
+++ src/axolotl/core/trainers/mixins/distributed_parallel.py
@@ -4,8 +4,6 @@ Mixin for correctly saving fsdp
 
 from transformers import Trainer
 
-from axolotl.utils.distributed import barrier
-
 
 class DistributedParallelMixin(Trainer):
     """
@@ -20,4 +18,3 @@ class DistributedParallelMixin(Trainer):
         ):
             state_dict = self.accelerator.get_state_dict(self.model)
         super()._save(output_dir, state_dict=state_dict)
-        barrier()  # sometimes non-rank0 processes will continue training while rank0 is saving optimizer
