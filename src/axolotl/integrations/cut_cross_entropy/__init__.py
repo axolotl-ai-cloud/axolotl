91b3472ca41c05445a6d9a9d2c56b69e890860c7
NanoCode012 <nano@axolotl.ai>
2025-08-08 10:19:49 +0700

    chore: update cce commit

--- src/axolotl/integrations/cut_cross_entropy/__init__.py
+++ src/axolotl/integrations/cut_cross_entropy/__init__.py
@@ -34,7 +34,7 @@ LOG = get_logger(__name__)
 
 _CCE_INSTALL_MESSAGE = (
     "Please install Axolotl's fork of cut_cross_entropy with transformers support using "
-    '`pip install "cut-cross-entropy[transformers] @ git+https://github.com/axolotl-ai-cloud/ml-cross-entropy.git@bb8d9f8"`'
+    '`pip install "cut-cross-entropy[transformers] @ git+https://github.com/axolotl-ai-cloud/ml-cross-entropy.git@0ee9ee8"`'
 )
 
 
