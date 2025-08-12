df3bf4150df77d107cec1f79f847648e278bbadd
Salman Mohammadi <salman.mohammadi@outlook.com>
2025-08-12 09:38:30 +0100

    add tkps

--- src/axolotl/utils/callbacks/tokens_per_second.py
+++ src/axolotl/utils/callbacks/tokens_per_second.py
@@ -1,28 +1,55 @@
 import time
-from transformers import TrainerCallback, TrainingArguments, TrainerState, TrainerControl
+
+from transformers import (
+    TrainerCallback,
+    TrainerControl,
+    TrainerState,
+    TrainingArguments,
+)
+
 
 class StepTimingCallback(TrainerCallback):
     """
     A callback to measure and log the duration of each training step.
     """
+
     def __init__(self):
         super().__init__()
         self.step_time = 0.0
         self.start_time = 0.0
 
-    def on_step_begin(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
+    def on_step_begin(
+        self,
+        args: TrainingArguments,
+        state: TrainerState,
+        control: TrainerControl,
+        **kwargs,
+    ):
         """
         Event called at the beginning of a training step.
         """
         self.start_time = time.perf_counter()
 
-    def on_step_end(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
+    def on_step_end(
+        self,
+        args: TrainingArguments,
+        state: TrainerState,
+        control: TrainerControl,
+        **kwargs,
+    ):
         """
         Event called at the end of a training step.
         """
         self.step_time = time.perf_counter() - self.start_time
 
-    def on_log(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, logs=None, **kwargs):
+    def on_log(
+        self,
+        args: TrainingArguments,
+        state: TrainerState,
+        control: TrainerControl,
+        logs=None,
+        **kwargs,
+    ):
         """
         Event called when logging is done.
         """
