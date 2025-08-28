c1909d81b231e34fb5ee2765343b611147e5684a
Dan Saunders <danjsaund@gmail.com>
2025-08-28 03:21:58 +0000

    moe kernels

--- src/axolotl/kernels/moe/__init__.py
+++ src/axolotl/kernels/moe/__init__.py
@@ -2,6 +2,6 @@
 Optimized MoE kernels for axolotl
 """
 
-from .triton_kernels import ContiguousGroupedGEMM, cg_grouped_gemm_forward
+from .kernels import ContiguousGroupedGEMM, cg_grouped_gemm_forward
 
 __all__ = ["ContiguousGroupedGEMM", "cg_grouped_gemm_forward"]
