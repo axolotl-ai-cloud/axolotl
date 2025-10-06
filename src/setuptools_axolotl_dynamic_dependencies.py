ca54b4c4e7ef4da23f5cee5d19f7254c704fd84e
Wing Lian <wing@axolotl.ai>
2025-10-06 11:07:14 -0400

    remove autoawq from setuptools script

--- src/setuptools_axolotl_dynamic_dependencies.py
+++ src/setuptools_axolotl_dynamic_dependencies.py
@@ -33,7 +33,6 @@ def parse_requirements():
     try:
         xformers_version = [req for req in _install_requires if "xformers" in req][0]
         torchao_version = [req for req in _install_requires if "torchao" in req][0]
-        autoawq_version = [req for req in _install_requires if "autoawq" in req][0]
 
         if "Darwin" in platform.system():
             # don't install xformers on MacOS
@@ -63,7 +62,6 @@ def parse_requirements():
                     _install_requires.append("xformers==0.0.28.post2")
                 else:
                     _install_requires.append("xformers==0.0.28.post3")
-                _install_requires.pop(_install_requires.index(autoawq_version))
             elif (major, minor) >= (2, 4):
                 if patch == 0:
                     _install_requires.pop(_install_requires.index(xformers_version))
