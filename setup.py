4b1b4fa6d86246cfef1e8b693c011bab2d7db7dd
Wing Lian <wing@axolotl.ai>
2025-10-30 10:03:24 -0400

    upgrade numpy (#3236)
    * upgrade numpy to 2.3.4
    
    * bump contribs for numpy
    
    * fix vllm versions
    
    * bump numba
    
    * make sure psutil is installed
    
    * add psutil to cicd dockerfile jinja
    
    * lower dep versions of numba + numpy for vllm
    
    * bump datasets version
    
    * resolve pydantic conflict too

--- setup.py
+++ setup.py
@@ -65,9 +65,11 @@ def parse_requirements(extras_require_map):
             if (major, minor) >= (2, 9):
                 extras_require_map.pop("fbgemm-gpu")
                 extras_require_map["fbgemm-gpu"] = ["fbgemm-gpu-genai==1.4.1"]
+                extras_require_map["vllm"] = ["vllm==0.11.1"]
             elif (major, minor) >= (2, 8):
                 extras_require_map.pop("fbgemm-gpu")
                 extras_require_map["fbgemm-gpu"] = ["fbgemm-gpu-genai==1.3.0"]
+                extras_require_map["vllm"] = ["vllm==0.11.0"]
             elif (major, minor) >= (2, 7):
                 _install_requires.pop(_install_requires.index(xformers_version))
                 if patch == 0:
@@ -76,7 +78,7 @@ def parse_requirements(extras_require_map):
                     extras_require_map.pop("vllm")
                 else:
                     _install_requires.append("xformers==0.0.31")
-                    extras_require_map["vllm"] = ["vllm>=0.10.0"]
+                    extras_require_map["vllm"] = ["vllm==0.10.1"]
             elif (major, minor) >= (2, 6):
                 _install_requires.pop(_install_requires.index(xformers_version))
                 _install_requires.append("xformers==0.0.29.post3")
