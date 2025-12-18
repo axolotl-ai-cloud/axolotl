3750d7dd64a2c0699d5009392d85de525f7414c6
VED <146507396+ved1beta@users.noreply.github.com>
2025-12-18 21:41:06 +0530

    add liger support kernal for dpo (#3302)
    * add liger kernal 4 dpo
    
    * revert grpo changes,add support in dpo
    
    * revert grpo changes,add support in dpo
    
    * dpo_use_liger_kernal
    
    * fix liger_dpo
    
    ---------
    
    Co-authored-by: Ved <ved.work2024@gmail.com>

--- src/axolotl/utils/schemas/config.py
+++ src/axolotl/utils/schemas/config.py
@@ -173,6 +173,12 @@ class AxolotlInputConfig(
     dpo_use_logits_to_keep: bool | None = None
     dpo_label_smoothing: float | None = None
     dpo_norm_loss: bool | None = None
+
+    dpo_use_liger_kernel: bool | None = Field(
+        default=None,
+        json_schema_extra={"description": "Whether to use Liger kernel for DPO loss."},
+    )
+
     dpo_padding_free: bool | None = None
     dpo_generate_during_eval: bool | None = None
 
