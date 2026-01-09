b2c12aa9f1bba13f577bbd5e098d807aed42299a
Juliana Nieto Cárdenas <jnietoca@purdue.edu>
2026-01-09 17:34:39 -0500

    changes from dataset_processes to dataset_num_proc

--- src/axolotl/utils/datasets.py
+++ src/axolotl/utils/datasets.py
@@ -1,12 +1,19 @@
 """helper functions for datasets"""
 
 import os
+from axolotl.utils.logging import get_logger
 
+LOG = get_logger(__name__)
 
 def get_default_process_count():
     if axolotl_dataset_num_proc := os.environ.get("AXOLOTL_DATASET_NUM_PROC"):
         return int(axolotl_dataset_num_proc)
     if axolotl_dataset_processes := os.environ.get("AXOLOTL_DATASET_PROCESSES"):
+        # A deprecation warning as dataset_num_proc is standard and use, and dataset_processes is not
+        LOG.warning(
+            "AXOLOTL_DATASET_PROCESSES and dataset_processes is deprecated" \
+            "Please use `dataset_num_proc` instead"
+        )
         return int(axolotl_dataset_processes)
     if runpod_cpu_count := os.environ.get("RUNPOD_CPU_COUNT"):
         return int(runpod_cpu_count)
