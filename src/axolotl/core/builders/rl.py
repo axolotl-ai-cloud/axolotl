19649736f0ab110904b540205c252c1814a585e5
Wing Lian <wing@axolotl.ai>
2026-02-12 14:54:43 -0500

    cpoconfig doesn't take max_prompt_length and fix cpu offload

--- src/axolotl/core/builders/rl.py
+++ src/axolotl/core/builders/rl.py
@@ -134,13 +134,7 @@ class HFRLTrainerBuilder(TrainerBuilderBase):
             if self.cfg.cpo_alpha is not None:
                 training_args_kwargs["cpo_alpha"] = self.cfg.cpo_alpha
 
-            # Handle when max_prompt_length == max_length from defaults
-            # CPOTrainer requires strictly less than
-            if (
-                training_args_kwargs["max_prompt_length"]
-                == training_args_kwargs["max_length"]
-            ):
-                training_args_kwargs["max_prompt_length"] -= 1
+            blocklist_args_kwargs.append("max_prompt_length")
 
         elif self.cfg.rl is RLType.ORPO:
             training_args_cls = AxolotlORPOConfig
