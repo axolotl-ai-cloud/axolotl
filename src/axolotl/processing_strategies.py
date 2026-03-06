88a093ef0e9f1fea6c1544b3129c82a03029a942
ved1beta <vedantthote2019@gmail.com>
2026-03-06 17:44:49 +0530

    3_5_moe

--- src/axolotl/processing_strategies.py
+++ src/axolotl/processing_strategies.py
@@ -588,7 +588,7 @@ def get_processing_strategy(
         return Qwen2VLProcessingStrategy(
             **processing_kwargs,
         )
-    if chat_template_type == "qwen3_5":
+    if chat_template_type in ["qwen3_5", "qwen3_5_moe"]:
         return Qwen3_5ProcessingStrategy(
             **processing_kwargs,
         )
