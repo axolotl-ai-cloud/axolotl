6c92b5c31c8205ad68bb80d1530dfa8c1f680fc0
Wing Lian <wing@axolotl.ai>
2026-04-01 13:29:04 -0400

    lazy load trainer classes to prevent unnecesary imports (#3568)
    * lazy load trainer classes to prevent unnecesary imports
    
    * make the lazy load a common util

--- src/axolotl/core/trainers/__init__.py
+++ src/axolotl/core/trainers/__init__.py
@@ -2,15 +2,34 @@
 
 # flake8: noqa
 
+from axolotl.utils import make_lazy_getattr
+
 from .base import AxolotlTrainer
-from .dpo.trainer import AxolotlDPOTrainer
-from .ebft.strided import AxolotlStridedEBFTTrainer
-from .ebft.trainer import AxolotlEBFTTrainer
-from .mamba import AxolotlMambaTrainer
-from .trl import (
-    AxolotlCPOTrainer,
-    AxolotlKTOTrainer,
-    AxolotlORPOTrainer,
-    AxolotlPRMTrainer,
-    AxolotlRewardTrainer,
-)
+
+# noinspection PyUnresolvedReferences
+__all__ = [
+    "AxolotlTrainer",
+    "AxolotlCPOTrainer",
+    "AxolotlDPOTrainer",
+    "AxolotlEBFTTrainer",
+    "AxolotlKTOTrainer",
+    "AxolotlMambaTrainer",
+    "AxolotlORPOTrainer",
+    "AxolotlPRMTrainer",
+    "AxolotlRewardTrainer",
+    "AxolotlStridedEBFTTrainer",
+]
+
+_LAZY_IMPORTS = {
+    "AxolotlDPOTrainer": ".dpo.trainer",
+    "AxolotlStridedEBFTTrainer": ".ebft.strided",
+    "AxolotlEBFTTrainer": ".ebft.trainer",
+    "AxolotlMambaTrainer": ".mamba",
+    "AxolotlCPOTrainer": ".trl",
+    "AxolotlKTOTrainer": ".trl",
+    "AxolotlORPOTrainer": ".trl",
+    "AxolotlPRMTrainer": ".trl",
+    "AxolotlRewardTrainer": ".trl",
+}
+
+__getattr__ = make_lazy_getattr(_LAZY_IMPORTS, __name__, globals())
