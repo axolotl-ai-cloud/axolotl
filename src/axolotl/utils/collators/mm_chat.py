6238173068f8ed66b0f99febfe3d12fe7e26060b
NanoCode012 <nano@axolotl.ai>
2026-04-30 20:59:48 +0700

    fix: regression `IndexError: too many indices for tensor of dimension 2`

--- src/axolotl/utils/collators/mm_chat.py
+++ src/axolotl/utils/collators/mm_chat.py
@@ -47,12 +47,10 @@ class MultiModalChatDataCollator(DataCollatorMixin):
             messages,
             add_generation_prompt=False,
             tokenize=True,
+            return_tensors="pt",
+            return_dict=True,
             chat_template=self.processing_strategy.chat_template,
-            processor_kwargs={
-                "return_tensors": "pt",
-                "padding": True,
-                "return_dict": True,
-            },
+            processor_kwargs={"padding": True},
         )
 
         # Process the labels
