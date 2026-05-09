71666f827e938179822d043c71daf3a629e08961
NanoCode012 <nano@axolotl.ai>
2026-05-09 20:57:57 +0700

    fix: block sonicmoe for now

--- src/axolotl/integrations/expert_parallel/experts_fn.py
+++ src/axolotl/integrations/expert_parallel/experts_fn.py
@@ -67,13 +67,14 @@ def _scattermoe_local(experts, recv_x, recv_topk_idx, recv_topk_weights):
 
 
 def _sonicmoe_local(experts, recv_x, recv_topk_idx, recv_topk_weights):
-    from axolotl.integrations.kernels.libs.sonicmoe.gemma4_experts import (
-        gemma4_sonicmoe_experts_forward,
-    )
+    raise NotImplementedError("Sonicmoe + EP is not yet properly implemented.")
+    # from axolotl.integrations.kernels.libs.sonicmoe.gemma4_experts import (
+    #     gemma4_sonicmoe_experts_forward,
+    # )
 
-    return gemma4_sonicmoe_experts_forward(
-        experts, recv_x, recv_topk_idx, recv_topk_weights
-    )
+    # return gemma4_sonicmoe_experts_forward(
+    #     experts, recv_x, recv_topk_idx, recv_topk_weights
+    # )
 
 
 _LOCAL_KERNELS = {
