02a724a06167d8e0fc3f5f7a3d3e17b7ea97aecc
NanoCode012 <nano@axolotl.ai>
2026-07-11 16:29:36 +0700

    feat(kernels): add quack fused gated-MLP integration (quack_mlp_kernel)

---
+++ src/axolotl/integrations/quack_kernels/plugin.py
@@ -0,0 +1,39 @@
+# Copyright 2024 Axolotl AI. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+QuACK kernels plugin for Axolotl.
+"""
+
+from axolotl.integrations.base import BasePlugin
+from axolotl.utils.logging import get_logger
+
+LOG = get_logger(__name__)
+
+
+class QuackKernelsPlugin(BasePlugin):
+    """
+    Plugin for QuACK (CuTe-DSL) kernels.
+    """
+
+    def get_input_args(self):
+        return "axolotl.integrations.quack_kernels.QuackKernelsArgs"
+
+    def post_model_load(self, cfg, model):
+        if not cfg.quack_mlp_kernel:
+            return
+
+        from .mlp import apply_quack_mlp
+
+        apply_quack_mlp(model, cfg)
