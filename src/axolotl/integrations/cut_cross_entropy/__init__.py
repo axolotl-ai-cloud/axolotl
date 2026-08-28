c05f0d86bcb865eecabfb4917c1da0268f97d498
NanoCode012 <nano@axolotl.ai>
2026-08-28 21:34:38 +0700

    chore: bump cut cross entropy pin for qwen4_exp support

--- src/axolotl/integrations/cut_cross_entropy/__init__.py
+++ src/axolotl/integrations/cut_cross_entropy/__init__.py
@@ -36,7 +36,7 @@ LOG = get_logger(__name__)
 
 _CCE_INSTALL_MESSAGE = (
     "Please install Axolotl's fork of cut_cross_entropy with transformers support using "
-    '`pip uninstall -y cut-cross-entropy && pip install "cut-cross-entropy[transformers] @ git+https://github.com/axolotl-ai-cloud/ml-cross-entropy.git@e947d90"`'
+    '`pip uninstall -y cut-cross-entropy && pip install "cut-cross-entropy[transformers] @ git+https://github.com/axolotl-ai-cloud/ml-cross-entropy.git@4dfa522"`'
 )
 
 
